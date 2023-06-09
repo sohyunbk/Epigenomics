6c8c38b143c0ada0a965dbf44ba30e172d939f79
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-06-09 13:29:31 -0400

    Update Frombdg_Tobw.sh

--- scATAC-seq/2_A619andBif3ToBif3Ref/BeforeAddLibraries/Frombdg_Tobw.sh
+++ scATAC-seq/2_A619andBif3ToBif3Ref/BeforeAddLibraries/Frombdg_Tobw.sh
@@ -11,7 +11,7 @@
 ml Anaconda3/2020.02
 source activate /home/sb14489/.conda/envs/Jbrowse
 
-FileList=`find "/scratch/sb14489/3.scATAC/4.Bif3Ref/5.Jbrowse_MACS2" -name "*_pileup_CPM.bdg" | sed 's|.*/||'`
+FileList=`find "/scratch/sb14489/3.scATAC/4.Bif3Ref/5.Jbrowse_MACS2" -name "*_pileup_CPM.bdg"`
 
 #for i in $FileList; do echo $i
 #for i in $FileList; do echo "${i/_treat_pileup_CPM.bdg/}"
