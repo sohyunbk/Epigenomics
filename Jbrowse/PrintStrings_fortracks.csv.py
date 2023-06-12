9c979365380a5c8e426a5b22005eec203e51af7b
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-06-12 09:31:46 -0400

    Update PrintStrings_fortracks.csv.py

--- Jbrowse/PrintStrings_fortracks.csv.py
+++ Jbrowse/PrintStrings_fortracks.csv.py
@@ -40,7 +40,7 @@ def get_parser():
 def BWFiles(Dir,BW,GithubDir,TiedName):
     for sFiles in glob.glob(Dir+"/*.bw"):
         FileNamewithE = os.path.basename(sFiles)
-        FileName = ".".join(FileNamewithE.split(".")[0])
+        FileName = FileNamewithE.split(".")[0]
         #SampleName = FileName.split("_")[0]
         #CellName = "_".join(FileName.split("_")[1:]).split(".")[0]
         #atac,3_bif3_Re2_G2_M,3_bif3_Re2_G2_M,scATACMutantsV3/G2_M,,,scATACMutantsV3/3_bif3_Re2_G2_M.bw,,,,,,,
@@ -51,3 +51,4 @@ if __name__ == "__main__":
     args = get_parser().parse_args()
     if args.bw != None:
         BWFiles(args.DirForFiles,args.bw,args.GitHubDir,args.TiedName)
+        #python PrintStrings_fortracks.csv.py -DirForFiles /scratch/sb14489/3.scATAC/4.Bif3Ref/5.Jbrowse_MACS2 -bw Yes -TiedName scATAC -GitHubDir atac
