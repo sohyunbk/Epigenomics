5ffd246094b7429e9db4b810e11370fb39ef1fda
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-06-12 09:18:23 -0400

    Create PrintStrings_fortracks.csv.py

---
+++ Jbrowse/PrintStrings_fortracks.csv.py
@@ -0,0 +1,45 @@
+# Don't need to attach any conda envs
+
+import argparse
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description=""
+    )
+    parser.add_argument(
+        "-DirForFiles",
+        "--DirForFiles",
+        help="DirForFiles",
+        required=True,
+        dest="DirForFiles",
+    )
+    parser.add_argument(
+            "-bw",
+            "--bw",
+            help="bw",
+            required=False,
+            dest="bw",
+    )
+    parser.add_argument(
+            "-TiedName",
+            "--TiedName",
+            help="TiedName",
+            required=False,
+            dest="TiedName",
+    )
+    args = vars(parser.parse_args())
+    return parser
+
+def BWFiles(Dir,BW,TiedName):
+    for sFiles in glob.glob(BringFilesPath+"*.bw"):
+        FileNamewithE = os.path.basename(sFiles)
+        FileName = ".".join(FileNamewithE.split(".")[0])
+        #SampleName = FileName.split("_")[0]
+        #CellName = "_".join(FileName.split("_")[1:]).split(".")[0]
+        cmd = "%s,%s,%s,%s,,,,,,,,,,"%(GithubDir,FileNamewithE,FileNamewithE,TiedName)
+        print(cmd)
+
+if __name__ == "__main__":
+    args = get_parser().parse_args()
+    if args.bw != None:
+        BWFiles(args.DirForFiles,args.bw,args.TiedName)
