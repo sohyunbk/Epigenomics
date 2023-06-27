9fd9574dd7c2ff1ecf15fb7929184a5fc277d07d
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-06-27 15:29:20 -0400

    ss

--- scATAC-seq/2_A619andBif3ToBif3Ref/BeforeAddLibraries/GrepTheSamfileLines_UsingSamtools_Try2.sh
+++ scATAC-seq/2_A619andBif3ToBif3Ref/BeforeAddLibraries/GrepTheSamfileLines_UsingSamtools_Try2.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-#SBATCH --job-name=Try        # Job name
+#SBATCH --job-name=Try2        # Job name
 #SBATCH --partition=batch        # Partition (queue) name
 #SBATCH --ntasks=1                    # Run a single task
 #SBATCH --cpus-per-task=30            # Number of CPU cores per task
 #SBATCH --mem=80gb                   # Job memory request
 #SBATCH --time=60:00:00               # Time limit hrs:min:sec
-#SBATCH --output=/scratch/sb14489/0.log/Try.%j.out   # Standard output log
-#SBATCH --error=/scratch/sb14489/0.log/Try.%j.err    # Standard error log
+#SBATCH --output=/scratch/sb14489/0.log/Try2.%j.out   # Standard output log
+#SBATCH --error=/scratch/sb14489/0.log/Try2.%j.err    # Standard error log
 
 module load SAMtools/1.16.1-GCC-11.3.0
 #!/bin/bash
-Path="/scratch/sb14489/3.scATAC/4.Bif3Ref/6.Compare_Reads_inTwoRegions/"
 # Read A.txt line by line
 #!/bin/bash
 
-cd $Path
+
 # Input files
 #samtools view -@ 30 /scratch/sb14489/3.scATAC/4.Bif3Ref/3.SortedBam/3_bif3_2_Markingpcr.bam "chr2:3679079-3679413"
-samtools view -@ 30  --output-fmt SAM -h /scratch/sb14489/3.scATAC/4.Bif3Ref/3.SortedBam/3_bif3_2_Markingpcr.bam  \
- -L ZmWUS1PromoterRegions.bed \
- -o Bif3Re2_ToBif3Ref.sam
+samtools index -@  30 /scratch/sb14489/3.scATAC/4.Bif3Ref/3.SortedBam/3_bif3_2_Markingpcr.bam
+samtools view -@ 30 /scratch/sb14489/3.scATAC/4.Bif3Ref/3.SortedBam/3_bif3_2_Markingpcr.bam \
+ "chr2:3679079-3679413" > /scratch/sb14489/3.scATAC/4.Bif3Ref/3.SortedBam/3_bif3_2_Markingpcr.chr2_3679079_3679413
