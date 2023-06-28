e48db5b90e4cdf26edf4338fedfd20f511b88b43
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-06-28 17:10:48 -0400

    Update GrepTheSamfileLines_fromSelectedPeak_ToRef.sh

--- scATAC-seq/2_A619andBif3ToBif3Ref/BeforeAddLibraries/GrepTheSamfileLines_fromSelectedPeak_ToRef.sh
+++ scATAC-seq/2_A619andBif3ToBif3Ref/BeforeAddLibraries/GrepTheSamfileLines_fromSelectedPeak_ToRef.sh
@@ -22,14 +22,14 @@ input_file_b="/scratch/sb14489/3.scATAC/4.Bif3Ref/4.Bam_FixingBarcode/3_bif3_2_B
 output_file="Bif3_Re2_ToA619Ref_ZmWUS1PromoterPeak.sam"
 
 # Extract unique values from the 6th column of A.txt and create a pattern file
-awk '{print $6}' "$input_file_a" | sort | uniq > patterns.txt
+awk '{print $6}' "$input_file_a" | sort | uniq > patterns2.txt
 
 # Define the number of parallel processes
 num_processes=30
 
 # Search for matching lines in B.sam using multiple parallel processes
 xargs -P "$num_processes" -a patterns.txt -I {} grep -w {} "$input_file_b" >> "$output_file"
-xargs -P 3 -a patterns.txt -I {} grep -w {} Bif3_Re2_ToA619Ref_ZmWUS1PromoterPeak.sam
+xargs -P 3 -a patterns2.txt -I {} grep -w {} Bif3_Re2_ToA619Ref_ZmWUS1PromoterPeak.sam
 
 # Cleanup: Remove the temporary pattern file
 rm "$Path"patterns.txt
