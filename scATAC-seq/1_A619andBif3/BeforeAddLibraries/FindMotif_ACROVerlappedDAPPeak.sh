cd00664309e05e5f34e498992a9821709493b6d6
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-07-11 16:47:58 -0400

    Update FindMotif_ACROVerlappedDAPPeak.sh

--- scATAC-seq/1_A619andBif3/BeforeAddLibraries/FindMotif_ACROVerlappedDAPPeak.sh
+++ scATAC-seq/1_A619andBif3/BeforeAddLibraries/FindMotif_ACROVerlappedDAPPeak.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=FindMotifs        # Job name
 #SBATCH --partition=batch             # Partition (queue) name
 #SBATCH --ntasks=1                    # Run a single task
-#SBATCH --cpus-per-task=12             # Number of CPU cores per task
-#SBATCH --mem=50gb                   # Job memory request
+#SBATCH --cpus-per-task=1             # Number of CPU cores per task
+#SBATCH --mem=90gb                   # Job memory request
 #SBATCH --time=10:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/sb14489/0.log/FindMotifs.%j.out   # Standard output log
 #SBATCH --error=/scratch/sb14489/0.log/FindMotifs.%j.err    # Standard error log
