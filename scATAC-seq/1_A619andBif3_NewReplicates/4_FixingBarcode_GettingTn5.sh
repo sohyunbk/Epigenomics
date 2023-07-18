0d14bc545bbd35417350e161d001d7e48a51b182
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-07-18 12:55:13 -0400

    Update 4_FixingBarcode_GettingTn5.sh

--- scATAC-seq/1_A619andBif3_NewReplicates/4_FixingBarcode_GettingTn5.sh
+++ scATAC-seq/1_A619andBif3_NewReplicates/4_FixingBarcode_GettingTn5.sh
@@ -10,13 +10,15 @@
 #SBATCH --mail-type=BEGIN,END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --array=0-3
 
+SampleNameList=(A619_Re3 A619_Re4 bif3_Re3 bif3_Re4)
+
+
 module load Anaconda3/2020.02
 source activate /home/sb14489/.conda/envs/ucsc
 
 module load  SAMtools/1.10-iccifort-2019.5.281
 samtools index -@ 20 /scratch/sb14489/3.scATAC/2.Maize_ear/3.SortedBam/"${SampleNameList[SLURM_ARRAY_TASK_ID]}"_Rmpcr.bam
 
-SampleNameList=(A619_Re3 A619_Re4 bif3_Re3 bif3_Re4)
 python /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/FindTn5Insertion_fromBam.py  \
 -BAM //scratch/sb14489/3.scATAC/2.Maize_ear/3.SortedBam/"${SampleNameList[SLURM_ARRAY_TASK_ID]}"_Rmpcr.bam \
 -exp_name "${SampleNameList[SLURM_ARRAY_TASK_ID]}" -threads 20 \
