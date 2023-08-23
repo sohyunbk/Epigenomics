a93fee9c092f3524d315171d14bdf65e35446c6e
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-08-23 16:02:30 -0400

    Update 5-3_sbatch_CleanFeatureDetectDoublet_Mt0.05.sh

--- scATAC-seq/1_A619andBif3_NewReplicates/5-3_sbatch_CleanFeatureDetectDoublet_Mt0.05.sh
+++ scATAC-seq/1_A619andBif3_NewReplicates/5-3_sbatch_CleanFeatureDetectDoublet_Mt0.05.sh
@@ -11,11 +11,11 @@
 #SBATCH --mail-user=Sohyun.Bang@uga.edu  # Where to send mail
 #SBATCH --array=0-7                   # Array range
 
-List=(1_A619 1_A619_2 2_rel2 2_rel2_2 3_bif3 3_bif3_2  4_relk1 4_relk1_2)
+List=(A619_Re3 A619_Re4 bif3_Re3 bif3_Re4)
 
 ml Anaconda3/2020.02
 source activate r_env
 
-Rscript /home/sb14489/1.scATAC-seq/1_scATAC-seq/0_CoreScript/5_CellClustering_CombineLater/5-3_CleanFeature_DetectDoublets.R \
- --WD /scratch/sb14489/3.scATAC/2.Maize_ear/6.CellClustering/Organelle5Per_CombineLater/ \
+Rscript /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/CleanFeatures_DetectDoublets.R \
+ --WD /scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/AfterMtMapping/ \
  --Name "${List[SLURM_ARRAY_TASK_ID]}" --PreFix Tn5Cut1000_Binsize500_Mt0.05 --MinT 0.001 --MaxT 0.005 --nPC 100
