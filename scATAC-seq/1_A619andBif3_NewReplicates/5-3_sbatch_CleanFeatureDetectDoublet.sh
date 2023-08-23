c0cd546369c974fda4fd72013c2ac8de872c8a24
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-08-23 16:14:57 -0400

    ss

--- scATAC-seq/1_A619andBif3_NewReplicates/5-3_sbatch_CleanFeatureDetectDoublet.sh
+++ scATAC-seq/1_A619andBif3_NewReplicates/5-3_sbatch_CleanFeatureDetectDoublet.sh
@@ -4,12 +4,12 @@
 #SBATCH --ntasks=1                    # Run a single task
 #SBATCH --cpus-per-task=10             # Number of CPU cores per task
 #SBATCH --mem=100gb                   # Job memory request ## Should have more than 300 here
-#SBATCH --time=2:00:00               # Time limit hrs:min:sec
-#SBATCH --output=/scratch/sb14489/0.log/5-3_QC.%j.out   # Standard output log
-#SBATCH --error=/scratch/sb14489/0.log/5-3_QC.%j.err    # Standard error log
+#SBATCH --time=3:00:00               # Time limit hrs:min:sec
+#SBATCH --output=/scratch/sb14489/0.log/5-3_CleanFeatureDetectDoublet.%j.out   # Standard output log
+#SBATCH --error=/scratch/sb14489/0.log/5-3_CleanFeatureDetectDoublet.%j.err    # Standard error log
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=Sohyun.Bang@uga.edu  # Where to send mail
-#SBATCH --array=0-7                   # Array range
+#SBATCH --array=0-3                   # Array range
 
 List=(A619_Re3 A619_Re4 bif3_Re3 bif3_Re4)
 
@@ -18,4 +18,4 @@ source activate r_env
 
 Rscript /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/CleanFeatures_DetectDoublets.R \
  --WD /scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/AfterMtMapping/ \
- --Name "${List[SLURM_ARRAY_TASK_ID]}" --PreFix Tn5Cut1000_Binsize500_Mt0.05 --MinT 0.001 --MaxT 0.005 --nPC 100
+ --Name "${List[SLURM_ARRAY_TASK_ID]}" --PreFix Tn5Cut1000_Binsize500 --MinT 0.001 --MaxT 0.005 --nPC 100
