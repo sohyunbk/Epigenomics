0d1fd615c62b9600e03c0ee40f000f80ffc21beb
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-08-25 14:40:13 -0400

    Update 5-3_sbatch_CleanFeatureDetectDoublet.sh

--- scATAC-seq/1_A619andBif3_NewReplicates/5-3_sbatch_CleanFeatureDetectDoublet.sh
+++ scATAC-seq/1_A619andBif3_NewReplicates/5-3_sbatch_CleanFeatureDetectDoublet.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=highmem_p         # Partition (queue) name
 #SBATCH --ntasks=1                    # Run a single task
 #SBATCH --cpus-per-task=10             # Number of CPU cores per task
-#SBATCH --mem=100gb                   # Job memory request ## Should have more than 300 here
-#SBATCH --time=3:00:00               # Time limit hrs:min:sec
+#SBATCH --mem=300gb                   # Job memory request ## Should have more than 300 here
+#SBATCH --time=6:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/sb14489/0.log/5-3_CleanFeatureDetectDoublet.%j.out   # Standard output log
 #SBATCH --error=/scratch/sb14489/0.log/5-3_CleanFeatureDetectDoublet.%j.err    # Standard error log
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
@@ -18,4 +18,4 @@ source activate r_env
 
 Rscript /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/CleanFeatures_DetectDoublets.R \
  --WD /scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/AfterMtMapping/ \
- --Name "${List[SLURM_ARRAY_TASK_ID]}" --PreFix Tn5Cut1000_Binsize500 --MinT 0.001 --MaxT 0.005 --nPC 100
+ --Name "${List[SLURM_ARRAY_TASK_ID]}" --PreFix Tn5Cut1000_Binsize500 --MinT 0.01 --MaxT 0.05 --nPC 100
