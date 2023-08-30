951c1b60cbe20dc49ef1b8e2ba6ce26c9a9ae932
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-08-30 15:18:55 -0400

    Update 5-5_Submit.sh

--- scATAC-seq/1_A619andBif3_NewReplicates/5-5_Submit.sh
+++ scATAC-seq/1_A619andBif3_NewReplicates/5-5_Submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=Clustering        # Job name
-#SBATCH --partition=highmem_p         # Partition (queue) name
+#SBATCH --partition=schmitz_hm_p         # Partition (queue) name
 #SBATCH --ntasks=1                    # Run a single task
 #SBATCH --cpus-per-task=1             # Number of CPU cores per task
 #SBATCH --mem=300gb                   # Job memory request ## Should have more than 300 here
