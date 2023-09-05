658beb0033c2821bd4f8b20a0c2931111d13be86
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-09-04 21:19:58 -0400

    Update 6-2_MarkerGene_tfidf_bif3.sh

--- scATAC-seq/4_Original_Plus_Additional_A619Bif3/6-2_MarkerGene_tfidf_bif3.sh
+++ scATAC-seq/4_Original_Plus_Additional_A619Bif3/6-2_MarkerGene_tfidf_bif3.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1                    # Run a single task
 #SBATCH --cpus-per-task=40             # Number of CPU cores per task
 #SBATCH --mem=90gb                   # Job memory request ## Should have more than 300 here
-#SBATCH --time=0:42:00               # Time limit hrs:min:sec ## Tales ;ole 20-30min
+#SBATCH --time=0:40:00               # Time limit hrs:min:sec ## Tales ;ole 20-30min
 #SBATCH --output=/scratch/sb14489/0.log/6_MarkerGene.%j.out   # Standard output log
 #SBATCH --error=/scratch/sb14489/0.log/6_MarkerGene.%j.err    # Standard error log
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
@@ -16,7 +16,7 @@ source activate r_env
 Rscript /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/MarkerGenes_Tfidf_SubmitScript.R \
  --WD /scratch/sb14489/3.scATAC/2.Maize_ear/6.Annotation/1.MarkerGene/bif3_Re1Re2Re3Re4 \
  --Name bif3_Re1Re2Re3Re4 \
- --meta /scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/4Replicates/bif3/bif3_Re1234_FeaturesN165518_k50_res0.9.AfterHarmony.metadata.txt \
+ --meta /scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/4Replicates/bif3/bif3_Re1234_FeaturesN163511_k50_res0.9.AfterHarmony.metadata.txt \
  --geneact /scratch/sb14489/3.scATAC/2.Maize_ear/4.Bam_FixingBarcode/GA_bif3_Re1Re2Re3Re4_withUpdatedGTF_sorted.txt \
  --pcs /scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/4Replicates/bif3/bif3_Re1234.AfterHarmony.PCA.txt \
  --markers /scratch/sb14489/3.scATAC/0.Data/MarkerGene/230426_EarMarker.txt
