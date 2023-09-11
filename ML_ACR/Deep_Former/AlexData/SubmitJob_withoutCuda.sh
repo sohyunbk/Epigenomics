51d1b345e28b2aa65183a0e9ec6f57793ed41e0d
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-09-11 16:29:59 -0400

    Update SubmitJob_withoutCuda.sh

--- ML_ACR/Deep_Former/AlexData/SubmitJob_withoutCuda.sh
+++ ML_ACR/Deep_Former/AlexData/SubmitJob_withoutCuda.sh
@@ -19,7 +19,7 @@ cd /scratch/sb14489/8.ML_ACR/2.DeepFormer_DanQ
 #ymllist=(/home/sb14489/ACR_ML_caQTLs/Deep_Former/AlexData/500bp_MappableRegions_DanQ_withoutCuda_CellTypeRestrict.yml /home/sb14489/ACR_ML_caQTLs/Deep_Former/AlexData/500bp_MappableRegions_DanQ_withoutCuda_RemoveRedundantACR.yml /home/sb14489/ACR_ML_caQTLs/Deep_Former/AlexData/500bp_MappableRegions_DanQ_withoutCuda_RemoveRedundantACR_Try500bp.yml)
 #learningRatelist=(0.0005 0.0005 0.0005)
 
-ymllist=(/home/sb14489/ACR_ML_caQTLs/Deep_Former/AlexData/500bp_MappableRegions_DanQ_withoutCuda_CellTypeRestrict.yml)
+ymllist=(/home/sb14489/Epigenomics/ML_ACR/Deep_Former/AlexData/500bp_MappableRegions_DanQ_withoutCuda_FiveClasses.yml)
 learningRatelist=(0.0005)
 
 /home/sb14489/miniconda3/envs/pytorch/bin/python /home/sb14489/ACR_ML_caQTLs/Deep_Former/AlexData/Run.py \
