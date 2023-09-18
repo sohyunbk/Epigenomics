1852e4960e39e9baf638c1abf0eea7766b6494c9
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-09-18 17:21:48 -0400

    Update XStreme_Output_Summarize.py

--- scATAC-seq/0_CoreScript/Meme/XStreme_Output_Summarize.py
+++ scATAC-seq/0_CoreScript/Meme/XStreme_Output_Summarize.py
@@ -5,9 +5,35 @@ OutputXSteme="/scratch/sb14489/3.scATAC/2.Maize_ear/15.MEME_Motif/IM_OC_dACR_JAS
 #1-AGRGARRGAGAGAGA       STREME-1        2.1e-025        chr7:10333751-10334308  20.87   tp      0
 #1-AGRGARRGAGAGAGA       STREME-1        2.1e-025        chr10:4279363-4280396   20.87   tp      0
 
+def Open_Peak_bed(InfileName):
+    infile = open(InfileName,"r")
+    List = []
+    for sLine in infile:
+        sList = sLine.strip().split("\t")
+        Pos = sList[0]+":"+sList[1]+"-"+sList[2]
+        List.append(Pos)
+    return(List)
 
 infile = open(OutputXSteme,"r")
 infile.readline()
 
 Dic_byMotif={}
 for sLine in infile:
+    sList = sLine.strip().split("\t")
+    if len(sList)>2:
+        Dic_byMotif[sList[1]].append(sList[3])
+infile.close()
+
+A619Higher = Open_Peak_bed("/scratch/sb14489/3.scATAC/2.Maize_ear/11.dACRs/A619_vs_Bif3_BiggerPeaks_AllIntergenic_SeedOn/IM-OC.A619Higher.Bed")
+Bif3Higher = Open_Peak_bed("/scratch/sb14489/3.scATAC/2.Maize_ear/11.dACRs/A619_vs_Bif3_BiggerPeaks_AllIntergenic_SeedOn/IM-OC.Bif3Higher.Bed")
+
+nA619Higher = 0
+nBif3Higher = 0
+for i in Dic_byMotif["STREME-1"]:
+    if i in A619Higher:
+        nA619Higher+=1
+    else if i in Bif3Higher:
+        nBif3Higher+=1
+print(len(Dic_byMotif["STREME-1"]))
+print(nA619Higher)
+print(nBif3Higher)        
