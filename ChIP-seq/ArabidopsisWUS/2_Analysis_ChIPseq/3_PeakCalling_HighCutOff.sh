21ec86c20366d1a033c4237f504f1ca48122f779
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-09-26 16:31:55 -0400

    Update 3_PeakCalling_HighCutOff.sh

--- ChIP-seq/ArabidopsisWUS/2_Analysis_ChIPseq/3_PeakCalling_HighCutOff.sh
+++ ChIP-seq/ArabidopsisWUS/2_Analysis_ChIPseq/3_PeakCalling_HighCutOff.sh
@@ -15,7 +15,7 @@ module load MACS2/2.2.7.1-foss-2021b
 macs2 callpeak \
  -t /scratch/sb14489/7.DAPorChIP/CHIPseq_Ara_WUS/2.Mapped/SRR8192660_unique_bowtie2_algn.bam \
 	-c /scratch/sb14489/7.DAPorChIP/CHIPseq_Ara_WUS/2.Mapped/SRR8192661_unique_bowtie2_algn.bam \
- 	-f BAM -g 1.1e+8  --nomodel   --extsize 100 -q  0.005 \
+ 	-f BAM -g 1.1e+8  --nomodel   --extsize 100 -q  0.001 \
 	-n WUS_GS_NonModel \
 	--outdir /scratch/sb14489/7.DAPorChIP/CHIPseq_Ara_WUS/3.PeakCalling_HighCutoff
 #1000,000,000
