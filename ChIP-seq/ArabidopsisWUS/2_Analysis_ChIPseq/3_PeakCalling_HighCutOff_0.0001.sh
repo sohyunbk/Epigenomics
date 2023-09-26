be270ece14d2284da74169379165f91d9fd44e64
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-09-26 17:23:15 -0400

    Update 3_PeakCalling_HighCutOff_0.0001.sh

--- ChIP-seq/ArabidopsisWUS/2_Analysis_ChIPseq/3_PeakCalling_HighCutOff_0.0001.sh
+++ ChIP-seq/ArabidopsisWUS/2_Analysis_ChIPseq/3_PeakCalling_HighCutOff_0.0001.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=macs2        # Job name
-#SBATCH --partition=highmem_p         # Partition (queue) name
+#SBATCH --partition=schmitz_hm_p         # Partition (queue) name
 #SBATCH --ntasks=1                    # Run a single task
 #SBATCH --cpus-per-task=1            # Number of CPU cores per task
 #SBATCH --mem=100gb                   # Job memory request #For normal fastq : 600gb
