b29ddd9150476bca94b0b2fabc95bf78a6ffe9c1
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-10-02 23:57:53 -0400

    Update SubmitJob_withoutCuda.sh

--- ML_ACR/A619_Ear/2_RunSelene/SubmitJob_withoutCuda.sh
+++ ML_ACR/A619_Ear/2_RunSelene/SubmitJob_withoutCuda.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name=withoutCUDA        # Job name
-#SBATCH --partition=highmem_p         # Partition (queue) name
+#SBATCH --partition=schmitz_hm_p         # Partition (queue) name
 #SBATCH --ntasks=1                    # Run a single task
 #SBATCH --cpus-per-task=32             # Number of CPU cores per task
 #SBATCH --mem=100gb                   # Job memory request
-#SBATCH --time=100:00:00               # Time limit hrs:min:sec
+#SBATCH --time=10:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/sb14489/0.log/withoutCUDA.%j.out   # Standard output log
 #SBATCH --error=/scratch/sb14489/0.log/withoutCUDA.%j.err    # Standard error log
 #SBATCH --array=0
 
-cd /scratch/sb14489/8.ML_ACR/2.DeepFormer_DanQ
-#ymllist=(500bp_MappableRegions_DanQ_withoutCuda.yml)
-#learningRatelist=(0.0005)
+cd /scratch/sb14489/8.ML_ACR/2.MaizeEar/2.Selene
 
-#ymllist=(500bp_AllGenome_withBigN.yml evaluate_test.yml)
-#learningRatelist=(0.01)
-
-#ymllist=(/home/sb14489/ACR_ML_caQTLs/Deep_Former/AlexData/500bp_MappableRegions_DanQ_withoutCuda_CellTypeRestrict.yml /home/sb14489/ACR_ML_caQTLs/Deep_Former/AlexData/500bp_MappableRegions_DanQ_withoutCuda_RemoveRedundantACR.yml /home/sb14489/ACR_ML_caQTLs/Deep_Former/AlexData/500bp_MappableRegions_DanQ_withoutCuda_RemoveRedundantACR_Try500bp.yml)
-#learningRatelist=(0.0005 0.0005 0.0005)
-
-ymllist=(/home/sb14489/Epigenomics/ML_ACR/Deep_Former/AlexData/500bp_MappableRegions_DanQ_withoutCuda_FiveClasses.yml)
+ymllist=(/home/sb14489/Epigenomics/ML_ACR/A619_Ear/2_RunSelene/DanQ_A619.yml)
 learningRatelist=(0.0005)
 
 /home/sb14489/miniconda3/envs/pytorch/bin/python /home/sb14489/ACR_ML_caQTLs/Deep_Former/AlexData/Run.py \
