70d27691ceb471b5f8bd8a4ea56a0c67cc3e2dd6
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-10-03 00:04:31 -0400

    ss

--- ML_ACR/A619_Ear/1_MakeInputData/1_MakingBedInputByCellTypes.py
+++ ML_ACR/A619_Ear/1_MakeInputData/1_MakingBedInputByCellTypes.py
@@ -3,7 +3,7 @@ from pybedtools import BedTool
 Path = "/scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V3_RemoveFakePeak/A619/"
 CommonPeakFile = Path+"A619.500bp_peaks.FilteredOrgs.bed"
 CommonPeak = BedTool(CommonPeakFile)
-CellTypes = ["BundleSheath_VascularSchrenchyma","ProcambialMeristem_ProtoXylem_MetaXylem","SPM-base_SM-base","IM-OC","IM_SPM_SM","XylemParenchyma_PithParenchyma","ProtoPhloem_MetaPhloem_CompanionCell_PhloemParenchyma","CalloseRelated","FloralMeristem_SuppressedBract","L1atFloralMeristem","PhloemPrecursor"]
+CellTypes = ["G2_M","L1","BundleSheath_VascularSchrenchyma","ProcambialMeristem_ProtoXylem_MetaXylem","SPM-base_SM-base","IM-OC","IM_SPM_SM","XylemParenchyma_PithParenchyma","ProtoPhloem_MetaPhloem_CompanionCell_PhloemParenchyma","CalloseRelated","FloralMeristem_SuppressedBract","L1atFloralMeristem","PhloemPrecursor"]
 outfile = open("/scratch/sb14489/8.ML_ACR/2.MaizeEar/1.InputBed/MaizeEar_500bpPeak_A619_byCT.bed","w")
 for CT in CellTypes:
     FileName = Path+CT+"/A619_"+CT+".reproducible_summits_Onlychr"
