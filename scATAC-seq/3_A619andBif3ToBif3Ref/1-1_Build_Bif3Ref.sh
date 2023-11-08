2a79a0448946201ab38552a1a1b0140a30f7332d
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-11-08 15:57:08 -0500

    Update 1-1_Build_Bif3Ref.sh

--- scATAC-seq/3_A619andBif3ToBif3Ref/1-1_Build_Bif3Ref.sh
+++ scATAC-seq/3_A619andBif3ToBif3Ref/1-1_Build_Bif3Ref.sh
@@ -7,8 +7,8 @@
 #SBATCH --output=/scratch/sb14489/0.log/BuildRef.%j.out   # Standard output log
 #SBATCH --error=/scratch/sb14489/0.log/BuildRef.%j.err    # Standard error log
 
-Path=/scratch/sb14489/0.Reference/Maize_B73
+Path=/scratch/sb14489/0.Reference/Maize_Ki3
 
 cd $Path
 module load CellRanger-ATAC/2.0.0
-cellranger-atac mkref --config=CellRangerv2-ATAC_Maizev5_Bif3.config ## In protocl they are saying it should be gtf but gff seems fine in v1 but in v2 it should be gtf
+cellranger-atac mkref --config=CellRangerv2-ATAC_Ki3_Bif3.config ## In protocl they are saying it should be gtf but gff seems fine in v1 but in v2 it should be gtf
