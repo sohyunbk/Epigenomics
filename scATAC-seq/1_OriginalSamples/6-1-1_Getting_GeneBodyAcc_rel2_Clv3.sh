677e0ff7a7e433469de087e5e1a0674819ddd850
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-11-14 16:56:05 -0500

    ss

--- scATAC-seq/1_OriginalSamples/6-1-1_Getting_GeneBodyAcc_rel2_Clv3.sh
+++ scATAC-seq/1_OriginalSamples/6-1-1_Getting_GeneBodyAcc_rel2_Clv3.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=MarkerGene        # Job name
+#SBATCH --job-name=GeneBodyA        # Job name
 #SBATCH --partition=highmem_p         # Partition (queue) name
 #SBATCH --ntasks=1                    # Run a single task
 #SBATCH --cpus-per-task=1             # Number of CPU cores per task
 #SBATCH --mem=90gb                   # Job memory request ## Should have more than 300 here
 #SBATCH --time=2:00:00               # Time limit hrs:min:sec
-#SBATCH --output=/scratch/sb14489/0.log/6_MarkerGene.%j.out   # Standard output log
-#SBATCH --error=/scratch/sb14489/0.log/6_MarkerGene.%j.err    # Standard error log
+#SBATCH --output=/scratch/sb14489/0.log/GeneBodyA.%j.out   # Standard output log
+#SBATCH --error=/scratch/sb14489/0.log/GeneBodyA.%j.err    # Standard error log
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=Sohyun.Bang@uga.edu  # Where to send mail
 
