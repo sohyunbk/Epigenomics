817ae01e2852211b7a060067bf04038c0144278a
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-11-14 14:23:09 -0500

    Update 2_Alignment_withHighCores.sh

--- scATAC-seq/3_A619andBif3ToBif3Ref/2_Alignment_withHighCores.sh
+++ scATAC-seq/3_A619andBif3ToBif3Ref/2_Alignment_withHighCores.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=Alignment        # Job name
-#SBATCH --partition=schmitz_hm_p         # Partition (queue) name
+#SBATCH --partition=highmem_p         # Partition (queue) name
 #SBATCH --ntasks=1                    # Run a single task
-#SBATCH --cpus-per-task=28             # Number of CPU cores per task
-#SBATCH --mem=360gb                   # Job memory request #For normal fastq : 600gb
+#SBATCH --cpus-per-task=20             # Number of CPU cores per task
+#SBATCH --mem=600gb                   # Job memory request #For normal fastq : 600gb
 #SBATCH --time=400:00:04               # Time limit hrs:min:sec #For normal fastq : 80 hours -for mapping CellRanger
 #SBATCH --output=/scratch/sb14489/0.log/2_Mapping.%j.out   # Standard output log
 #SBATCH --error=/scratch/sb14489/0.log/2_Mapping.%j.err    # Standard error log
@@ -11,7 +11,7 @@
 #SBATCH --array=0-3                   # Array range
 
 #sbatch /home/sb14489/1.scATAC-seq/1_scATAC-seq/0_CoreScript/2_Alignment/2-1_Alignment_CellRangerv2.sh
-cd /scratch/sb14489/3.scATAC/4.Bif3Ref_B73/2.Mapped_withHighCores
+cd /scratch/sb14489/3.scATAC/4.Bif3Ref_B73/2.Mapped_withHighMem
 
 Sample=(1_A619 3_bif3 1_A619_2 3_bif3_2)
 module load CellRanger-ATAC/2.1.0
