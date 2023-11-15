c927d84fe9bb3ac1bf32839a50a0b6abe781baaf
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-11-15 10:49:05 -0500

    Update 6-1-2_MarkerGene_tfidf_rel2_denovoMarkerGene.sh

--- scATAC-seq/1_OriginalSamples/6-1-2_MarkerGene_tfidf_rel2_denovoMarkerGene.sh
+++ scATAC-seq/1_OriginalSamples/6-1-2_MarkerGene_tfidf_rel2_denovoMarkerGene.sh
@@ -14,9 +14,9 @@ ml Anaconda3/2020.02
 source activate r_env
 
 Rscript /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/MarkerGenes_Tfidf_SubmitScript.R \
---WD /scratch/sb14489/3.scATAC/2.Maize_ear/6.Annotation/1.MarkerGene/rel2_DenovoMarkers \
---Name rel2_ZmCLE7 \
+--WD /scratch/sb14489/3.scATAC/2.Maize_ear/6.Annotation/1.MarkerGene/rel2_TopDenovoMarkersA619 \
+--Name rel2_TopDenovoMarkersA619 \
 --meta /scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/Organelle5Per_CombineLater/rel2/rel2_Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100_RemoveBLonlyMitoChloroChIP_k50_res0.9.AfterHarmony.metadata.txt \
 --geneact /scratch/sb14489/3.scATAC/2.Maize_ear/4.Bam_FixingBarcode/GA_rel2_includingZmCLE7.txt \
 --pcs /scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/Organelle5Per_CombineLater/rel2/rel2_Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100_RemoveBLonlyMitoChloroChIP.AfterHarmony.PCA.txt \
---markers /scratch/sb14489/3.scATAC/0.Data/MarkerGene/230426_EarMarker.txt
+--markers /scratch/sb14489/3.scATAC/0.Data/MarkerGene/231113_Top5DenovoGenesinA619.txt
