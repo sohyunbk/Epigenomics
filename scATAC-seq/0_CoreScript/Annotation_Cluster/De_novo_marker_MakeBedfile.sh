9045e97dab04878b863c03c2e2373c4b620cf9cc
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-11-15 13:11:29 -0500

    Update De_novo_marker_MakeBedfile.sh

--- scATAC-seq/0_CoreScript/Annotation_Cluster/De_novo_marker_MakeBedfile.sh
+++ scATAC-seq/0_CoreScript/Annotation_Cluster/De_novo_marker_MakeBedfile.sh
@@ -10,6 +10,14 @@
 #chr3	83813927	83840797	Zm00001eb132450	Zm00001eb132450_pval_0_G2_M	G2_M
 #chr1	260696094	260705322	Zm00001eb257620	Zm00001eb257620_pval_0_G2_M	G2_M
 
+#!/bin/bash
+
+# Check if the correct number of arguments was provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 input_file output_file"
+    exit 1
+fi
+
 # Assign arguments to variables
 input_file="$1"
 output_file="$2"
@@ -19,11 +27,11 @@ awk '
 BEGIN { FS=OFS="\t" }
 NR == 1 { print; next } # Print the header and skip to the next record
 {
-    # Change the 4th column to match the 3rd column
-    $4 = $3;
+    # Change the 5th column to match the 3rd column
+    $5 = $3;
 
-    # Create a key based on the first four columns
-    key = $1 FS $2 FS $3 FS $4;
+    # Create a key based on the first five columns
+    key = $1 FS $2 FS $3 FS $4 FS $5;
 
     # Concatenate types with a comma for matching keys
     if (key in data) {
@@ -36,7 +44,6 @@ END {
     # Print the merged rows
     for (key in data) {
         split(data[key], line, FS);
-        line[6] = data[key];
         print line[1], line[2], line[3], line[4], line[5], data[key];
     }
 }' "$input_file" > "$output_file"
