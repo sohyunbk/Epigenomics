230a5f7e8ba891f68c5d367d325a6cf0d2618cc5
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-11-17 16:00:13 -0500

    ss

--- scATAC-seq/1_OriginalSamples/6-0_DecidedCelltypes_rel2_withA619V4.R
+++ scATAC-seq/1_OriginalSamples/6-0_DecidedCelltypes_rel2_withA619V4.R
@@ -12,6 +12,8 @@ head(loaded_meta_data)
 SubClusterDir <- "/scratch/sb14489/3.scATAC/2.Maize_ear/6.Annotation/4.Subclustering/rel2/"
 Cluster1 <- read.table(paste0(SubClusterDir,"Cluster1_Sub_res1_knear100_Partmetadata.txt"))
 Cluster1$LouvainClusters <- paste0("1_",Cluster1$LouvainClusters)
+Cluster2 <- read.table(paste0(SubClusterDir,"Cluster2_Sub_res2_knear100_Partmetadata.txt"))
+Cluster2$LouvainClusters <- paste0("2_",Cluster3$LouvainClusters)
 Cluster3 <- read.table(paste0(SubClusterDir,"Cluster3_Sub_res1_knear100_Partmetadata.txt"))
 Cluster3$LouvainClusters <- paste0("3_",Cluster3$LouvainClusters)
 Cluster4 <- read.table(paste0(SubClusterDir,"Cluster4_Sub_res1_knear100_Partmetadata.txt"))
@@ -19,6 +21,7 @@ Cluster4$LouvainClusters <- paste0("4_",Cluster4$LouvainClusters)
 head(Cluster1)
 #levels(factor(loaded_meta_data$LouvainClusters))
 loaded_meta_data[rownames(Cluster1),]$LouvainClusters <- Cluster1$LouvainClusters
+loaded_meta_data[rownames(Cluster2),]$LouvainClusters <- Cluster2$LouvainClusters
 loaded_meta_data[rownames(Cluster3),]$LouvainClusters <- Cluster3$LouvainClusters
 loaded_meta_data[rownames(Cluster4),]$LouvainClusters <- Cluster4$LouvainClusters
 
@@ -40,25 +43,34 @@ NewMeta[which(NewMeta$LouvainClusters =="1_1"),]$Ann_v4 <- "Unknown1"
 NewMeta[which(NewMeta$LouvainClusters =="1_2"),]$Ann_v4 <- "Unknown_rel2"
 NewMeta[which(NewMeta$LouvainClusters =="1_3"),]$Ann_v4 <- "Unknown_rel2"
 NewMeta[which(NewMeta$LouvainClusters =="1_4"),]$Ann_v4 <- "Unknown1"
-NewMeta[which(NewMeta$LouvainClusters =="2"),]$Ann_v4 <- "PhloemPrecursor"
+
+NewMeta[which(NewMeta$LouvainClusters =="2_1"),]$Ann_v4 <- "G2_M"
+NewMeta[which(NewMeta$LouvainClusters =="2_2"),]$Ann_v4 <- "ProcambialMeristem_ProtoXylem_MetaXylem"
+NewMeta[which(NewMeta$LouvainClusters =="2_3"),]$Ann_v4 <- "PhloemPrecursor"
+NewMeta[which(NewMeta$LouvainClusters =="2_4"),]$Ann_v4 <- "PhloemPrecursor"
+NewMeta[which(NewMeta$LouvainClusters =="2_5"),]$Ann_v4 <- "PhloemPrecursor"
+NewMeta[which(NewMeta$LouvainClusters =="2_6"),]$Ann_v4 <- "PhloemPrecursor"
+NewMeta[which(NewMeta$LouvainClusters =="2_7"),]$Ann_v4 <- "PhloemPrecursor"
+NewMeta[which(NewMeta$LouvainClusters =="2_8"),]$Ann_v4 <- "PhloemPrecursor"
 
 NewMeta[which(NewMeta$LouvainClusters =="3_1"),]$Ann_v4 <- "Unknown_lowTn5"
 NewMeta[which(NewMeta$LouvainClusters =="3_2"),]$Ann_v4 <- "Unknown_Sclerenchyma"
 NewMeta[which(NewMeta$LouvainClusters =="3_3"),]$Ann_v4 <- "Unknown_Sclerenchyma"
 NewMeta[which(NewMeta$LouvainClusters =="3_4"),]$Ann_v4 <- "Unknown_Sclerenchyma"
+
 NewMeta[which(NewMeta$LouvainClusters =="4_1"),]$Ann_v4 <- "IM-OC"
 NewMeta[which(NewMeta$LouvainClusters =="4_2"),]$Ann_v4 <- "FloralMeristem_SuppressedBract"
 NewMeta[which(NewMeta$LouvainClusters =="4_3"),]$Ann_v4 <- "Unknown_lowTn5"
 
 
 NewMeta[which(NewMeta$LouvainClusters =="5"),]$Ann_v4 <- "SPM-base_SM-base"
-NewMeta[which(NewMeta$LouvainClusters =="6"),]$Ann_v4 <- ""
-NewMeta[which(NewMeta$LouvainClusters =="7"),]$Ann_v4 <- ""
-NewMeta[which(NewMeta$LouvainClusters =="8"),]$Ann_v4 <- ""
-NewMeta[which(NewMeta$LouvainClusters =="9"),]$Ann_v4 <- ""
-NewMeta[which(NewMeta$LouvainClusters =="10"),]$Ann_v4 <- ""
-NewMeta[which(NewMeta$LouvainClusters =="11"),]$Ann_v4 <- ""
-NewMeta[which(NewMeta$LouvainClusters =="12"),]$Ann_v4 <- ""
+NewMeta[which(NewMeta$LouvainClusters =="6"),]$Ann_v4 <- "L1"
+NewMeta[which(NewMeta$LouvainClusters =="7"),]$Ann_v4 <- "L1atFloralMeristem"
+NewMeta[which(NewMeta$LouvainClusters =="8"),]$Ann_v4 <- "XylemParenchyma_PithParenchyma"
+NewMeta[which(NewMeta$LouvainClusters =="9"),]$Ann_v4 <- "XylemParenchyma_PithParenchyma"
+NewMeta[which(NewMeta$LouvainClusters =="10"),]$Ann_v4 <- "G2_M"
+NewMeta[which(NewMeta$LouvainClusters =="11"),]$Ann_v4 <- "ProtoPhloem_MetaPhloem_CompanionCell_PhloemParenchyma"
+NewMeta[which(NewMeta$LouvainClusters =="12"),]$Ann_v4 <- "ProcambialMeristem_ProtoXylem_MetaXylem"
 
 levels(factor(NewMeta$Ann_v4))
 table(NewMeta$Ann_v4)
