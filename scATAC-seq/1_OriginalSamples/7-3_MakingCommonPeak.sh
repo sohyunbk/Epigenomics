35a5975e10bedce6e21484da9f034836c034fe7d
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-11-27 16:49:56 -0500

    Change

--- scATAC-seq/1_OriginalSamples/7-3_MakingCommonPeak.sh
+++ scATAC-seq/1_OriginalSamples/7-3_MakingCommonPeak.sh
@@ -7,18 +7,12 @@
 #SBATCH --time=5:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/sb14489/0.log/7_PeakCalling.%j.out   # Standard output log
 #SBATCH --error=/scratch/sb14489/0.log/7_PeakCalling.%j.err    # Standard error log
-#SBATCH --array=0-2
 
-Path=/scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4
-Combined=(1_A619 3_bif3 2_rel2)
-Sample=(A619 bif3 rel2)
-MetaData=(Ref_AnnV4_metadata.txt ShouldbeChanged rel2_AnnV4.txt)
 ml Anaconda3/2022.10
 source activate r_env
 
 cd $Path
-~/.conda/envs/r_env/bin/python /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/PeakCalling_byCellTypes/Call_scACRs_WithoutFakePeak.py \
- -bed /scratch/sb14489/3.scATAC/2.Maize_ear/4.Bam_FixingBarcode/"${Combined[SLURM_ARRAY_TASK_ID]}"_Combined_Sorted_k12.bed \
- -meta /scratch/sb14489/3.scATAC/2.Maize_ear/6.Annotation/0.AnnotatedMeta/"${Sample[SLURM_ARRAY_TASK_ID]}"/"${MetaData[SLURM_ARRAY_TASK_ID]}" \
- -col Ann_v4 -base "${Sample[SLURM_ARRAY_TASK_ID]}" -outdir "${Sample[SLURM_ARRAY_TASK_ID]}" \
- -fai /scratch/sb14489/0.Reference/Maize_B73/Zm-B73-REFERENCE-NAM-5.0_OnlyChr.fa.fai -bw TRUE
+~/.conda/envs/r_env/bin/python /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/PeakCalling_byCellTypes/MergingACRs_500bpFixed_withHighestTn5.py \
+ -Path1_forCellPeak /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/A619 \
+ -Path2_forCellPeak /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/rel2 \
+ -OutPutFile /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/A619_rel2_500bpCommonPeak/ComA619rel2.unique500bpPeaks.bed
--- scATAC-seq/1_OriginalSamples/7-4_ClassifyPeaks.sh
+++ scATAC-seq/1_OriginalSamples/7-4_ClassifyPeaks.sh
@@ -7,10 +7,6 @@
 #SBATCH --time=1:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/sb14489/0.log/ClassifyingPeaks.%j.out   # Standard output log
 #SBATCH --error=/scratch/sb14489/0.log/ClassifyingPeaks.%j.err    # Standard error log
-#SBATCH --array=0-1
-
-Sample=(A619 bif3)
-
 
 ml Anaconda3/2020.02
 source activate r_env
@@ -18,9 +14,9 @@ source activate r_env
 ## It makes bw file too!
 
 ~/.conda/envs/r_env/bin/python /home/sb14489/1.scATAC-seq/1_scATAC-seq/0_CoreScript/0_Functions/ClassifyPeaks_Intergenic_Genic.py \
- -PeakFile /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V3_RemoveFakePeak/"${Sample[SLURM_ARRAY_TASK_ID]}"/"${Sample[SLURM_ARRAY_TASK_ID]}".500bp_peaks.FilteredOrgs.bed  \
+ -PeakFile /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/A619_rel2_500bpCommonPeak/ComA619rel2.unique500bpPeaks.bed  \
  -BlackListFile /scratch/sb14489/0.Reference/Maize_B73/Zm.final_blaclist.Mito_Chloro_Chip.txt \
  -FaiFile /scratch/sb14489/0.Reference/Maize_B73/Zm-B73-REFERENCE-NAM-5.0_MtPtAdd_Rsf.fa.fai \
  -Ann_bed  /scratch/sb14489/0.Reference/Maize_B73/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1_OnlyGene_Chr.bed \
- -OutfilePath /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V3_RemoveFakePeak/"${Sample[SLURM_ARRAY_TASK_ID]}"/ \
- -OutfileName "${Sample[SLURM_ARRAY_TASK_ID]}".500bp_peaks
+ -OutfilePath /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/A619_rel2_500bpCommonPeak/ \
+ -OutfileName ComA619rel2.unique500bpPeaks
