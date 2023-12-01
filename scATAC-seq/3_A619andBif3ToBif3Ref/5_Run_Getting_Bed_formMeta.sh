374a07c13545c8c986c5ccacacf57d226d767113
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-12-01 16:43:57 -0500

    Update 5_Run_Getting_Bed_formMeta.sh

--- scATAC-seq/3_A619andBif3ToBif3Ref/5_Run_Getting_Bed_formMeta.sh
+++ scATAC-seq/3_A619andBif3ToBif3Ref/5_Run_Getting_Bed_formMeta.sh
@@ -10,11 +10,11 @@
 #SBATCH --array=0-3                  # Array range
 
  BedList=(1_A619_2_Unique.bed 1_A619_Unique.bed 3_bif3_2_Unique.bed 3_bif3_Unique.bed)
- MetaData=(Ref_AfterMt0.5Cutoff/Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100/Ref_AnnV3_metadata.txt Ref_AfterMt0.5Cutoff/Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100/Ref_AnnV3_metadata.txt  Organelle5Per_CombineLater/bif3/Bif3_AnnV3_metadata.txt Organelle5Per_CombineLater/bif3/Bif3_AnnV3_metadata.txt)
+ MetaData=(A619/Ref_AnnV4_metadata.txt A619/Ref_AnnV4_metadata.txt Bif3/Bif3_AnnV3_metadata.txt Bif3/Bif3_AnnV3_metadata.txt)
  OutFileList=(1_A619_Re2 1_A619_Re1 3_bif3_Re2 3_bif3_Re1)
 
- ~/.conda/envs/r_env/bin/python y \
- -BedFile /scratch/sb14489/3.scATAC/4.Bif3Ref/4.Bam_FixingBarcode/"${BedList[SLURM_ARRAY_TASK_ID]}" \
- -Outfile /scratch/sb14489/3.scATAC/4.Bif3Ref/5.Jbrowse_MACS2/"${OutFileList[SLURM_ARRAY_TASK_ID]}" \
- -MetaFile /scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/"${MetaData[SLURM_ARRAY_TASK_ID]}" \
- -GenomeSize /scratch/sb14489/0.Reference/Maize_B73/Zm-B73-REFERENCE-NAM-5.0_MtPtAdd_Rsf.fa.fai
+ ~/.conda/envs/r_env/bin/python /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/PeakCalling_byCellTypes/WithouPeakCalling_Getting_Bed_byCelltype_Macs2.py \
+ -BedFile /scratch/sb14489/3.scATAC/4.Bif3Ref_Ki3/4.Bam_FixingBarcode/"${BedList[SLURM_ARRAY_TASK_ID]}" \
+ -Outfile /scratch/sb14489/3.scATAC/4.Bif3Ref_Ki3/5.Jbrowse_MACS2/"${OutFileList[SLURM_ARRAY_TASK_ID]}" \
+ -MetaFile /scratch/sb14489/3.scATAC/2.Maize_ear/6.Annotation/0.AnnotatedMeta/"${MetaData[SLURM_ARRAY_TASK_ID]}" \
+ -GenomeSize /scratch/sb14489/0.Reference/Maize_Ki3/Zm-Ki3-REFERENCE-NAM-1.0_OnlyChr_Bif3.fa.fai
