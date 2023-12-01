c005854b18124c14a6a6d7f0892ec00e2e524805
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-12-01 16:21:54 -0500

    Change

--- scATAC-seq/3_A619andBif3ToBif3Ref/5_Run_Getting_Bed_formMeta.sh
+++ scATAC-seq/3_A619andBif3ToBif3Ref/5_Run_Getting_Bed_formMeta.sh
@@ -9,8 +9,12 @@
 #SBATCH --error=/scratch/sb14489/0.log/12_GettingBed.%j.err    # Standard error log
 #SBATCH --array=0-3                  # Array range
 
-BedList=(1_A619_2_Unique.bed 1_A619_Unique.bed 3_bif3_2_Unique.bed 3_bif3_Unique.bed)
-MetaData=(Ref_AfterMt0.5Cutoff/Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100/Ref_AnnV3_metadata.txt Ref_AfterMt0.5Cutoff/Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100/Ref_AnnV3_metadata.txt  Organelle5Per_CombineLater/bif3/Bif3_AnnV3_metadata.txt Organelle5Per_CombineLater/bif3/Bif3_AnnV3_metadata.txt)
-OutFileList=(1_A619_Re2 1_A619_Re1 3_bif3_Re2 3_bif3_Re1)
+ BedList=(1_A619_2_Unique.bed 1_A619_Unique.bed 3_bif3_2_Unique.bed 3_bif3_Unique.bed)
+ MetaData=(Ref_AfterMt0.5Cutoff/Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100/Ref_AnnV3_metadata.txt Ref_AfterMt0.5Cutoff/Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100/Ref_AnnV3_metadata.txt  Organelle5Per_CombineLater/bif3/Bif3_AnnV3_metadata.txt Organelle5Per_CombineLater/bif3/Bif3_AnnV3_metadata.txt)
+ OutFileList=(1_A619_Re2 1_A619_Re1 3_bif3_Re2 3_bif3_Re1)
 
-python /home/sb14489/1.scATAC-seq/1_scATAC-seq/0_CoreScript/12_CheckReplicates/1_Getting_Bed_fromMetaFile.py /scratch/sb14489/3.scATAC/2.Maize_ear/4.Bam_FixingBarcode/"${BedList[SLURM_ARRAY_TASK_ID]}" /scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/"${MetaData[SLURM_ARRAY_TASK_ID]}" /scratch/sb14489/3.scATAC/2.Maize_ear/12.Macs_GenomeBrowserByReplicates/"${OutFileList[SLURM_ARRAY_TASK_ID]}"
+ ~/.conda/envs/r_env/bin/python y \
+ -BedFile /scratch/sb14489/3.scATAC/4.Bif3Ref/4.Bam_FixingBarcode/"${BedList[SLURM_ARRAY_TASK_ID]}" \
+ -Outfile /scratch/sb14489/3.scATAC/4.Bif3Ref/5.Jbrowse_MACS2/"${OutFileList[SLURM_ARRAY_TASK_ID]}" \
+ -MetaFile /scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/"${MetaData[SLURM_ARRAY_TASK_ID]}" \
+ -GenomeSize /scratch/sb14489/0.Reference/Maize_B73/Zm-B73-REFERENCE-NAM-5.0_MtPtAdd_Rsf.fa.fai
