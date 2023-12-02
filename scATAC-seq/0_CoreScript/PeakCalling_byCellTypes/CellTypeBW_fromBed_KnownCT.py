611f6742772fd047c6b35c47053f55b8e3fad03b
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-12-02 17:33:38 -0500

    Update CellTypeBW_fromBed_KnownCT.py

--- scATAC-seq/0_CoreScript/PeakCalling_byCellTypes/CellTypeBW_fromBed_KnownCT.py
+++ scATAC-seq/0_CoreScript/PeakCalling_byCellTypes/CellTypeBW_fromBed_KnownCT.py
@@ -92,6 +92,32 @@ def process_bed_by_celltype(meta_file, bed_file, output_file_base):
 
 
 ## 2) Run Macs2
+def check_files_exist(meta_file, output_file_base):
+    """
+    Check if the output BED files for each cell type already exist.
+
+    :param meta_file: Path to the metadata file.
+    :param output_file_base: Base path for the expected output files.
+    :returns: A tuple (all_files_exist, list_of_files)
+    """
+    list_of_files = []
+    all_files_exist = True
+    with open(meta_file, "r") as infile:
+        infile.readline()  # Skip header
+        cell_types = set()
+        for sLine in infile:
+            cell_type = sLine.strip().split("\t")[-1]
+            cell_types.add(cell_type)
+
+    for cell_type in cell_types:
+        expected_file = f"{output_file_base}_{cell_type}.bed"
+        list_of_files.append(expected_file)
+        if not os.path.exists(expected_file):
+            all_files_exist = False
+
+    return all_files_exist, list_of_files
+
+
 def run_macs2_threaded(bed_files, output_directory, cores):
     with Pool(int(cores)) as pool:
             pool.map(
@@ -118,9 +144,15 @@ if __name__ == "__main__":
     args = get_parser().parse_args()
     BedFile = args.bed
     MetaFile = args.m
-    Outfile =args.Outfile
-    ### 1) First get bed file  by cell type
-    bed_files = process_bed_by_celltype(args.m, args.bed, args.Outfile)
-    print(bed_files)
-    run_macs2_threaded(bed_files, args.Outfile, args.cores)
-    ## 2) Run Macs2
+    Outfile = args.Outfile
+
+    ## 1) Make bed files
+    # Check if the bed files for each cell type already exist
+    files_exist, bed_files = check_files_exist(MetaFile, Outfile)
+
+    if not files_exist:
+        # Generate bed files if they don't exist
+        bed_files = process_bed_by_celltype(MetaFile, BedFile, Outfile)
+
+    # 2) Run Macs2
+    run_macs2_threaded(bed_files, Outfile, args.cores)
