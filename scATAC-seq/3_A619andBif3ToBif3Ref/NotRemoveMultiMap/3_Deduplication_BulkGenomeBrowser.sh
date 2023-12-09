b48e13052455c183e4afa976553a220d0c9fab5f
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-12-09 18:06:01 -0500

    Update 3_Deduplication_BulkGenomeBrowser.sh

--- scATAC-seq/3_A619andBif3ToBif3Ref/NotRemoveMultiMap/3_Deduplication_BulkGenomeBrowser.sh
+++ scATAC-seq/3_A619andBif3ToBif3Ref/NotRemoveMultiMap/3_Deduplication_BulkGenomeBrowser.sh
@@ -20,10 +20,10 @@ module load SAMtools/1.16.1-GCC-11.3.0
 module load BEDTools/2.29.2-GCC-8.3.0
 
 ## RemovemultiMap & deduplication
-#sh /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/Mapping_RefiningBam/RemoveMultiMap_Deduplication_PicardVersionUpdate.sh \
-# --path /scratch/sb14489/3.scATAC/4.Bif3Ref_Ki3_NotRemoveMultiMap/ --RemoveDup No \
-#--MappedDir 2.Mapped  --OGSampleName "${OGSampleNameList[SLURM_ARRAY_TASK_ID]}" \
-# --NewSampleName_forBam "${NewSampleNameList[SLURM_ARRAY_TASK_ID]}"
+sh /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/Mapping_RefiningBam/RemoveMultiMap_Deduplication_PicardVersionUpdate.sh \
+ --path /scratch/sb14489/3.scATAC/4.Bif3Ref_Ki3_NotRemoveMultiMap/ --RemoveDup No \
+--MappedDir 2.Mapped  --OGSampleName "${OGSampleNameList[SLURM_ARRAY_TASK_ID]}" \
+ --NewSampleName_forBam "${NewSampleNameList[SLURM_ARRAY_TASK_ID]}"
 
 ## GenomeBrowser
 module load Anaconda3/2022.10
