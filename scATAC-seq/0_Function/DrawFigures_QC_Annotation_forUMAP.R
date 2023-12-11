dae1c4d957cb84a8ff9b4d2ef1a3a88ff6aa18ba
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-12-11 12:39:10 -0500

    Update DrawFigures_QC_Annotation_forUMAP.R

--- scATAC-seq/0_Function/DrawFigures_QC_Annotation_forUMAP.R
+++ scATAC-seq/0_Function/DrawFigures_QC_Annotation_forUMAP.R
@@ -16,10 +16,11 @@ PreAnn <- ggplot(PreAnnMeta, aes(x=umap1, y=umap2, color=factor(LouvainClusters)
     labs(title = paste0("Pre-Annotation\n CellNumber: ",nrow(PreAnnMeta)),
          x = "UMAP1",
          y = "UMAP2")+
-    theme(axis.text.x = element_text(size = 25),  # Adjust size for x-axis text
-          axis.text.y = element_text(size = 25),
-          axis.title.x = element_text(size = 30),
-          axis.title.y = element_text(size = 30))
+    theme(legend.key.size = unit(3, "lines"),
+        axis.text.x = element_text(size = 25),  # Adjust size for x-axis text
+        axis.text.y = element_text(size = 25),
+        axis.title.x = element_text(size = 30),
+        axis.title.y = element_text(size = 30))
     
 Meta$Ann <- Meta[[Slot]]
 Meta$Ann <- factor(Meta$Ann,levels=(CellOrder))
@@ -35,7 +36,8 @@ All <- ggplot(Meta, aes(x=umap1, y=umap2, color=factor(Ann))) +
   labs(title = paste0("Re1+R2 \n CellNumber: ",nrow(Meta)),
        x = "UMAP1",
        y = "UMAP2")+
-  theme(axis.text.x = element_text(size = 25),  # Adjust size for x-axis text
+  theme(legend.key.size = unit(3, "lines"),
+        axis.text.x = element_text(size = 25),  # Adjust size for x-axis text
         axis.text.y = element_text(size = 25),
         axis.title.x = element_text(size = 30),
         axis.title.y = element_text(size = 30))
