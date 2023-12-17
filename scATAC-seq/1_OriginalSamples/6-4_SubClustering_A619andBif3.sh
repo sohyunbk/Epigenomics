9f2ce250225cd7ee88f71d94eb13a40c00c81788
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2023-12-16 20:33:27 -0500

    ss

---
+++ scATAC-seq/1_OriginalSamples/6-4_SubClustering_A619andBif3.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name=SubCluster        # Job name
+#SBATCH --partition=batch         # Partition (queue) name
+#SBATCH --ntasks=1                    # Run a single task
+#SBATCH --cpus-per-task=1             # Number of CPU cores per task
+#SBATCH --mem=70gb                   # Job memory request ## Should have more than 300 here
+#SBATCH --time=1:00:00               # Time limit hrs:min:sec
+#SBATCH --output=/scratch/sb14489/0.log/SubCluster.%j.out   # Standard output log
+#SBATCH --error=/scratch/sb14489/0.log/SubCluster.%j.err    # Standard error log
+#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=Sohyun.Bang@uga.edu  # Where to send mail
+#SBATCH --array=0-2
+
+source activate r_env
+SampleNames=(A169
+A169
+Bif3)
+
+MetaFiles=(
+/scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/Ref_AfterMt0.5Cutoff/Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100/Ref_RemoveBLonlyMitoChloroChIP.REF_CELLs.metadata.txt
+/scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/Ref_AfterMt0.5Cutoff/Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100/Ref_RemoveBLonlyMitoChloroChIP.REF_CELLs.metadata.txt
+/scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/Organelle5Per_CombineLater/bif3/bif3_Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100_RemoveBLonlyMitoChloroChIP_k50_res0.9.AfterHarmony.metadata.txt)
+
+RDS=(
+/scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/Ref_AfterMt0.5Cutoff/Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100/Ref_RemoveBLonlyMitoChloroChIP.afterHarmony.processed.rds
+/scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/Ref_AfterMt0.5Cutoff/Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100/Ref_RemoveBLonlyMitoChloroChIP.afterHarmony.processed.rds
+/scratch/sb14489/3.scATAC/2.Maize_ear/5.CellClustering/Organelle5Per_CombineLater/bif3/bif3_Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100_RemoveBLonlyMitoChloroChIP_k50_res0.9.AfterHarmony.rds)
+
+Cluesters=(1
+3
+1)
+
+module load CellRanger-ATAC/2.0.0
+
+Rscript /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/Annotation_Cluster/Subclustering.R \
+--SampleName "${SampleNames[SLURM_ARRAY_TASK_ID]}" \
+--MetaFile "${MetaFiles[SLURM_ARRAY_TASK_ID]}" \
+--ObjAfterHarmony "${RDS[SLURM_ARRAY_TASK_ID]}" \
+--AnnSlot LouvainClusters \
+--TargetClusterName "${Cluesters[SLURM_ARRAY_TASK_ID]}" \
+--OutputDir /scratch/sb14489/3.scATAC/2.Maize_ear/6.Annotation/4.Subclustering/"${SampleNames[SLURM_ARRAY_TASK_ID]}"
