f9732eaa3926ed516b5e4397c041d38d505cfdda
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-01-03 17:32:19 -0500

    Update 7-3_MakingCommon500bpPeak.sh

--- scATAC-seq/1_OriginalSamples/7-3_MakingCommon500bpPeak.sh
+++ scATAC-seq/1_OriginalSamples/7-3_MakingCommon500bpPeak.sh
@@ -7,12 +7,16 @@
 #SBATCH --time=5:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/sb14489/0.log/7_PeakCalling.%j.out   # Standard output log
 #SBATCH --error=/scratch/sb14489/0.log/7_PeakCalling.%j.err    # Standard error log
+#SBATCH --array=0-1
 
 ml Anaconda3/2022.10
 source activate r_env
 
-cd $Path
+Path1=(A619 A619)
+Path2=(Bif3 rel2)
+OutPutFilesNames=(A619_Bif3_500bpCommonPeak/ComA619Bif3.unique500bpPeaks.bed A619_rel2_500bpCommonPeak/ComA619rel2.unique500bpPeaks.bed)
+
 ~/.conda/envs/r_env/bin/python /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/PeakCalling_byCellTypes/MergingACRs_500bpFixed_withHighestTn5.py \
- -Path1_forCellPeak /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/A619 \
- -Path2_forCellPeak /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/rel2 \
- -OutPutFile /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/A619_rel2_500bpCommonPeak/ComA619rel2.unique500bpPeaks.bed
+ -Path1_forCellPeak /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/"${Path1[SLURM_ARRAY_TASK_ID]}" \
+ -Path2_forCellPeak /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/"${Path2[SLURM_ARRAY_TASK_ID]}" \
+ -OutPutFile /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/"${OutPutFilesNames[SLURM_ARRAY_TASK_ID]}"
