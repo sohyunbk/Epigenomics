cc8aefbc88cc7f503d9c4b8703b8ccafc33644b4
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-01-04 17:07:03 -0500

    Update 7-4_ClassifyPeaks.sh

--- scATAC-seq/1_OriginalSamples/7-4_ClassifyPeaks.sh
+++ scATAC-seq/1_OriginalSamples/7-4_ClassifyPeaks.sh
@@ -7,16 +7,20 @@
 #SBATCH --time=1:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/sb14489/0.log/ClassifyingPeaks.%j.out   # Standard output log
 #SBATCH --error=/scratch/sb14489/0.log/ClassifyingPeaks.%j.err    # Standard error log
+#SBATCH --array=0-1
 
 ml Anaconda3/2020.02
 source activate r_env
 
+OutPutFilesDir=(A619_Bif3_500bpCommonPeak A619_rel2_500bpCommonPeak)
+OutPutFiles=(ComA619Bif3.unique500bpPeaks ComA619rel2.unique500bpPeaks)
+
 ## It makes bw file too!
 
 ~/.conda/envs/r_env/bin/python /home/sb14489/1.scATAC-seq/1_scATAC-seq/0_CoreScript/0_Functions/ClassifyPeaks_Intergenic_Genic.py \
- -PeakFile /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/A619_rel2_500bpCommonPeak/ComA619rel2.unique500bpPeaks.bed  \
+ -PeakFile /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/"${OutPutFilesDir[SLURM_ARRAY_TASK_ID]}"/"${OutPutFiles[SLURM_ARRAY_TASK_ID]}".bed  \
  -BlackListFile /scratch/sb14489/0.Reference/Maize_B73/Zm.final_blaclist.Mito_Chloro_Chip.txt \
- -FaiFile /scratch/sb14489/0.Reference/Maize_B73/Zm-B73-REFERENCE-NAM-5.0_MtPtAdd_Rsf.fa.fai \
+ -FaiFile /scratch/sb14489/0.Reference/Maize_B73/Zm-B73-REFERENCE-NAM-5.0_OnlyChr.fa.fai \
  -Ann_bed  /scratch/sb14489/0.Reference/Maize_B73/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1_OnlyGene_Chr.bed \
- -OutfilePath /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/A619_rel2_500bpCommonPeak/ \
- -OutfileName ComA619rel2.unique500bpPeaks
+ -OutfilePath /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/"${OutPutFilesDir[SLURM_ARRAY_TASK_ID]}"/ \
+ -OutfileName "${OutPutFiles[SLURM_ARRAY_TASK_ID]}"
