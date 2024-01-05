c44481c255318a76812d62e6930a667051f9f0d3
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-01-05 12:48:59 -0500

    Update 7-5_UploadBWFilesbyCTfromPeakCall.sh

--- scATAC-seq/1_OriginalSamples/7-5_UploadBWFilesbyCTfromPeakCall.sh
+++ scATAC-seq/1_OriginalSamples/7-5_UploadBWFilesbyCTfromPeakCall.sh
@@ -17,13 +17,13 @@ Bif3
 rel2
 )
 
-find /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/"${WorkingDirs[SLURM_ARRAY_TASK_ID]}" \
- -name "*.macs_treat_pileup.normalized.bdg" -exec mv {} /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/BWFiles \;
+#find /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/"${WorkingDirs[SLURM_ARRAY_TASK_ID]}" \
+# -name "*.macs_treat_pileup.normalized.bdg" -exec cp {} /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/BWFiles \;
 
 cd /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/BWFiles
 
 for file in "${WorkingDirs[SLURM_ARRAY_TASK_ID]}"*.bdg; do
-    bedSort $file ${file%.bdg}.sorted.bdg
+    sort -k1,1 -k2,2n $file ${file%.bdg}.sorted.bdg
     bedGraphToBigWig ${file%.bdg}.sorted.bdg ${file%.bdg}.sorted.bw
 done
 
