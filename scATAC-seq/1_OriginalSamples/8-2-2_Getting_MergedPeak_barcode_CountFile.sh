77ab09b09e552ea79a0b8b51684430607ee188af
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-01-07 12:48:49 -0500

    Update 8-2-2_Getting_MergedPeak_barcode_CountFile.sh

--- scATAC-seq/1_OriginalSamples/8-2-2_Getting_MergedPeak_barcode_CountFile.sh
+++ scATAC-seq/1_OriginalSamples/8-2-2_Getting_MergedPeak_barcode_CountFile.sh
@@ -9,6 +9,7 @@
 #SBATCH --error=/scratch/sb14489/0.log/11.%j.err    # Standard error log
 #SBATCH --array=0-27
 
+## 14 number of cell types
 ClusterN=(L1atFloralMeristem  Unknown1 FloralMeristem_SuppressedBract  PhloemPrecursor  Unknown2
 G2_M ProcambialMeristem_ProtoXylem_MetaXylem  Unknown_Sclerenchyma IM-OC ProtoPhloem_MetaPhloem_CompanionCell_PhloemParenchyma
 Unknown_lowFRiP L1 SPM-base_SM-base  XylemParenchyma_PithParenchyma
@@ -16,19 +17,29 @@ L1atFloralMeristem  Unknown1 FloralMeristem_SuppressedBract  PhloemPrecursor  Un
 G2_M ProcambialMeristem_ProtoXylem_MetaXylem  Unknown_Sclerenchyma IM-OC ProtoPhloem_MetaPhloem_CompanionCell_PhloemParenchyma
 Unknown_lowFRiP L1 SPM-base_SM-base  XylemParenchyma_PithParenchyma)
 
+bedFiles=(1_A619_Combined_Sorted_k12.bed_OnlyChr 1_A619_Combined_Sorted_k12.bed_OnlyChr 1_A619_Combined_Sorted_k12.bed_OnlyChr
+1_A619_Combined_Sorted_k12.bed_OnlyChr 1_A619_Combined_Sorted_k12.bed_OnlyChr 1_A619_Combined_Sorted_k12.bed_OnlyChr
+1_A619_Combined_Sorted_k12.bed_OnlyChr 1_A619_Combined_Sorted_k12.bed_OnlyChr 1_A619_Combined_Sorted_k12.bed_OnlyChr
+1_A619_Combined_Sorted_k12.bed_OnlyChr 1_A619_Combined_Sorted_k12.bed_OnlyChr 1_A619_Combined_Sorted_k12.bed_OnlyChr
+1_A619_Combined_Sorted_k12.bed_OnlyChr 1_A619_Combined_Sorted_k12.bed_OnlyChr
+3_bif3_Combined_Sorted_k12.bed_OnlyChr 3_bif3_Combined_Sorted_k12.bed_OnlyChr 3_bif3_Combined_Sorted_k12.bed_OnlyChr
+3_bif3_Combined_Sorted_k12.bed_OnlyChr 3_bif3_Combined_Sorted_k12.bed_OnlyChr 3_bif3_Combined_Sorted_k12.bed_OnlyChr
+ 3_bif3_Combined_Sorted_k12.bed_OnlyChr 3_bif3_Combined_Sorted_k12.bed_OnlyChr 3_bif3_Combined_Sorted_k12.bed_OnlyChr
+ 3_bif3_Combined_Sorted_k12.bed_OnlyChr 3_bif3_Combined_Sorted_k12.bed_OnlyChr 3_bif3_Combined_Sorted_k12.bed_OnlyChr
+ 3_bif3_Combined_Sorted_k12.bed_OnlyChr 3_bif3_Combined_Sorted_k12.bed_OnlyChr
+)
+OutFileName=(byA619Barcode byA619Barcode byA619Barcode byA619Barcode byA619Barcode byA619Barcode byA619Barcode byA619Barcode
+byA619Barcode byA619Barcode byA619Barcode byA619Barcode byA619Barcode byA619Barcode
+byBif3Barcode byBif3Barcode byBif3Barcode byBif3Barcode byBif3Barcode byBif3Barcode byBif3Barcode byBif3Barcode
+byBif3Barcode byBif3Barcode byBif3Barcode byBif3Barcode byBif3Barcode byBif3Barcode)
+
 ml Anaconda3/2023.09-0
 source activate /home/sb14489/.conda/envs/Jbrowse
 
 module load BEDTools/2.30.0-GCC-10.2.0
 
-bedtools intersect -a /scratch/sb14489/3.scATAC/2.Maize_ear/4.Bam_FixingBarcode/1_A619_Combined_Sorted_k12.bed_OnlyChr  \
+bedtools intersect -a /scratch/sb14489/3.scATAC/2.Maize_ear/4.Bam_FixingBarcode/"${bedFiles[SLURM_ARRAY_TASK_ID]}"  \
   -b /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/A619_Bif3_MergedDifferentSizePeak/A619Bif3_"${ClusterN[SLURM_ARRAY_TASK_ID]}"_MergedPeak_Intergenic.bed  \
   -wa -wb \
-  -sorted  | perl ~/1.scATAC-seq/1_scATAC-seq/0_CoreScript/8_FindCommonACRPos/8-3_fastSparse.nonbinary.peak.pl \
-   - > /scratch/sb14489/3.scATAC/2.Maize_ear/8.Comparative_Analysis/2.dACR/A619_vs_Bif3_AnnV4/"${ClusterN[SLURM_ARRAY_TASK_ID]}"_PeaksCount_byA619Barcode.txt
-
-bedtools intersect -a /scratch/sb14489/3.scATAC/2.Maize_ear/4.Bam_FixingBarcode/3_bif3_Combined_Sorted_k12.bed_OnlyChr  \
- -b /scratch/sb14489/3.scATAC/2.Maize_ear/7.PeakCalling/Ann_V4/A619_Bif3_MergedDifferentSizePeak/A619Bif3_"${ClusterN[SLURM_ARRAY_TASK_ID]}"_MergedPeak_Intergenic.bed  \
- -wa -wb \
- -sorted  | perl ~/1.scATAC-seq/1_scATAC-seq/0_CoreScript/8_FindCommonACRPos/8-3_fastSparse.nonbinary.peak.pl \
-  - > /scratch/sb14489/3.scATAC/2.Maize_ear/8.Comparative_Analysis/2.dACR/A619_vs_Bif3_AnnV4/"${ClusterN[SLURM_ARRAY_TASK_ID]}"_PeaksCount_byBif3Barcode.txt
+  -sorted  | perl /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/dACR/FastSparse.nonbinary.peak.pl \
+   - > /scratch/sb14489/3.scATAC/2.Maize_ear/8.Comparative_Analysis/2.dACR/A619_vs_Bif3_AnnV4/"${ClusterN[SLURM_ARRAY_TASK_ID]}"_PeaksCount_"${OutFileName[SLURM_ARRAY_TASK_ID]}".txt
