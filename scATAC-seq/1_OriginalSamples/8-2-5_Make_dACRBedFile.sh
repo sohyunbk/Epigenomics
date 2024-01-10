af7f7442d9f0e446ebe15f095fe98bbd19e2b436
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-01-10 10:15:08 -0500

    Update 8-2-5_Make_dACRBedFile.sh

--- scATAC-seq/1_OriginalSamples/8-2-5_Make_dACRBedFile.sh
+++ scATAC-seq/1_OriginalSamples/8-2-5_Make_dACRBedFile.sh
@@ -13,4 +13,4 @@ source activate r_env
 
 Rscript /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/dACR/MakeBedfile_fromdACRResult.R \
 --DEGDir /scratch/sb14489/3.scATAC/2.Maize_ear/8.Comparative_Analysis/2.dACR/A619_vs_Bif3_AnnV4/ \
---DEGFileName  .EdgeRResult_PseudoReplicate_withPromoterRegion.txt \
+--DEGFileName  .EdgeRResult_PseudoReplicate_withPromoterRegion.txt 
