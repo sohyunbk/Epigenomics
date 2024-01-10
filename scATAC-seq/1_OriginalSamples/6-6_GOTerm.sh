d69f1bab8f1cd9d50447f6057fa17da03712f031
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-01-10 10:00:45 -0500

    Update 6-6_GOTerm.sh

--- scATAC-seq/1_OriginalSamples/6-6_GOTerm.sh
+++ scATAC-seq/1_OriginalSamples/6-6_GOTerm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Meme_motif        # Job name
+#SBATCH --job-name=GOTerm        # Job name
 #SBATCH --partition=highmem_p         # Partition (queue) name
 #SBATCH --ntasks=1                    # Run a single task
 #SBATCH --cpus-per-task=1             # Number of CPU cores per task
@@ -15,7 +15,7 @@ ml Anaconda3/2022.10
 source activate r_env
 
 Rscript /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/Annotation_Cluster/GOTerm_forDenovoMarkers.R \
- --OutPutDir /scratch/sb14489/3.scATAC/2.Maize_ear/6.Annotation/6.GOTerm/AnnV4 \
+ --OutPutDir /scratch/sb14489/3.scATAC/2.Maize_ear/6.Annotation/6.GOTerm/AnnV4/ \
  --FileNameFix _deseq_2_results.tsv \
  --WDir_forDenovo /scratch/sb14489/3.scATAC/2.Maize_ear/6.Annotation/3.Denovo/AnnV4/ \
  --GOData /scratch/sb14489/0.Reference/Maize_B73/GOTerm/B73_GO.RemoveBlank.out
