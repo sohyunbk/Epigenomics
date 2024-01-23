43ab387e105f0f75bdb10ffa5be666e2301afd71
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-01-23 16:44:24 -0500

    ss

--- ML_ACR/AlexData/Selene_Run/Submit_DanQ_500bp.sh
+++ ML_ACR/AlexData/Selene_Run/Submit_DanQ_500bp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=withoutCUDA        # Job name
-#SBATCH --partition=schmitz_hm_p         # Partition (queue) name
+#SBATCH --partition=highmem_p         # Partition (queue) name
 #SBATCH --ntasks=1                    # Run a single task
 #SBATCH --cpus-per-task=20             # Number of CPU cores per task
 #SBATCH --mem=80gb                   # Job memory request
