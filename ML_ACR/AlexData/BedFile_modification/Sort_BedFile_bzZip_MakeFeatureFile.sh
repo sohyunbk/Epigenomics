4d3c40a537c98a4d56c6021282c2b87f1a9e5f8d
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-01-23 16:39:59 -0500

    Update Sort_BedFile_bzZip_MakeFeatureFile.sh

--- ML_ACR/AlexData/BedFile_modification/Sort_BedFile_bzZip_MakeFeatureFile.sh
+++ ML_ACR/AlexData/BedFile_modification/Sort_BedFile_bzZip_MakeFeatureFile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=withoutCUDA        # Job name
-#SBATCH --partition=batch         # Partition (queue) name
+#SBATCH --partition=schmitz_hm_p         # Partition (queue) name
 #SBATCH --ntasks=1                    # Run a single task
 #SBATCH --cpus-per-task=1             # Number of CPU cores per task
 #SBATCH --mem=10gb                   # Job memory request
