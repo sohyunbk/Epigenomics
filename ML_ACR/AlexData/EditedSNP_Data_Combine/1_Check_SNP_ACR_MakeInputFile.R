cc3bea39b3aba48483793971a07d575f94e7c824
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-02-01 17:26:06 -0500

    s

--- ML_ACR/AlexData/EditedSNP_Data_Combine/1_Check_SNP_ACR_MakeInputFile.R
+++ ML_ACR/AlexData/EditedSNP_Data_Combine/1_Check_SNP_ACR_MakeInputFile.R
@@ -31,27 +31,36 @@ UniqueACR_Test <- SNPFile_test_Filtered %>%
   group_by(acrID) %>%
   sample_n(size = 1) %>%
   ungroup()
+dim(UniqueACR_Test)
 
 write.table(UniqueACR_Test, file = "/scratch/sb14489/8.ML_ACR/1.MaizeGenotypes_Alex/0.SNPData/test_SNVs.v2.curated.txt", sep = "\t", row.names = FALSE, quote=F)
 
-#### 2) Make Input bed file! :)
+#### ****** Control File
 ### Check common ACRs
-head(randomly_selected_control$acrID)
-head(ACRs_pos1NonRe) 
-head(randomly_selected_control$acrID)
-length(intersect(randomly_selected_control$acrID, ACRs_pos_NonRe))
-length(intersect(merged_SNPFile_test$acrID, ACRs_pos_NonRe))
-SelectedACR_Control <- intersect(randomly_selected_control$acrID, ACRs_pos_NonRe)
-SelectedACR_Test <- intersect(merged_SNPFile_test$acrID, ACRs_pos_NonRe)
-sum(!grepl("^chr10", SelectedACR_Test))
-SelectedACR_Control_SameNumberwithTest <- sample(SelectedACR_Control, size = length(intersect(merged_SNPFile_test$acrID, ACRs_pos_NonRe)))
-head(SelectedACR_Control_SameNumberwithTest)
+SNPFile <- read.table("/scratch/sb14489/8.ML_ACR/1.MaizeGenotypes_Alex/0.SNPData/control_SNVs.v2.txt", header=TRUE)
+unique_SNPFile <- unique(SNPFile)
+print(nrow(SNPFile))
+print(nrow(unique_SNPFile))
+head(unique_SNPFile)
+## 1) Check the overlap as it's extended with 1000bp
+split_pos <- strsplit(unique(SNPFile$acrID), "_")
+chr <- sapply(split_pos, function(x) x[1])
+start <- as.integer(sapply(split_pos, function(x) x[2])) -500
+end <- as.integer(sapply(split_pos, function(x) x[3])) +500
+gr <- GRanges(seqnames = chr, ranges = IRanges(start = start, end = end))
+overlaps <- findOverlaps(gr)
+summary(overlaps)
+overlap_pairs <- as.data.frame(overlaps)
+overlap_counts <- countOverlaps(gr)
+NonOverlappedACRs <- unique(SNPFile_test$acrID)[overlap_counts==1]
+head(NonOverlappedACRs)
+dim(SNPFile_test)
+SNPFile_Filtered <- SNPFile[SNPFile$acrID %in% NonOverlappedACRs,]
+dim(SNPFile_Filtered)
 
-head(ACRs_18cells)
-ACRs_18cells$V5 <- paste(ACRs_18cells$V1, ACRs_18cells$V2, ACRs_18cells$V3, sep="_")
-bedfile_SelectedACR_Test <- ACRs_18cells[ACRs_18cells$V5 %in% SelectedACR_Test,]
-bedfile_SelectedACR_control <- ACRs_18cells[ACRs_18cells$V5 %in% SelectedACR_Control_SameNumberwithTest,]
+UniqueACR <- SNPFile_Filtered %>%
+  group_by(acrID) %>%
+  sample_n(size = 1) %>%
+  ungroup()
 
-head(bedfile_SelectedACR_Test)
-write.table(bedfile_SelectedACR_Test[ , -5], file = "/scratch/sb14489/8.ML_ACR/1.MaizeGenotypes_Alex/0.SNPData/test_SNVs_curated_RandomSelectSNPperACR.bed", sep = "\t", col.names = FALSE, row.names = FALSE, quote=F)
-write.table(bedfile_SelectedACR_control[ , -5], file = "/scratch/sb14489/8.ML_ACR/1.MaizeGenotypes_Alex/0.SNPData/control_SNVs_curated_RandomSelectSNPperACR.bed", sep = "\t", col.names = FALSE, row.names = FALSE, quote=F)
+write.table(UniqueACR, file = "/scratch/sb14489/8.ML_ACR/1.MaizeGenotypes_Alex/0.SNPData/control_SNVs.v2.curated.txt", sep = "\t", row.names = FALSE, quote=F)
---
+++ ML_ACR/AlexData/EditedSNP_Data_Combine/2_Submit_MakeFastaFile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=MakeMutantFasta        # Job name
+#SBATCH --partition=batch         # Partition (queue) name
+#SBATCH --ntasks=1                    # Run a single task
+#SBATCH --cpus-per-task=1             # Number of CPU cores per task
+#SBATCH --mem=50gb                   # Job memory request
+#SBATCH --time=5:00:00               # Time limit hrs:min:sec
+#SBATCH --output=/scratch/sb14489/0.log/MakeMutantFasta.%j.out   # Standard output log
+#SBATCH --error=/scratch/sb14489/0.log/MakeMutantFasta.%j.err    # Standard error log
+#SBATCH --array=0-1
+
+
+Fasta=(test_SNVs.v2.fa control_SNVs.v2.curated.fa)
+SNPFiles=(test_SNVs.v2.curated.txt control_SNVs.v2.curated.txt)
+
+~/miniconda3/envs/pytorch/bin/python /home/sb14489/Epigenomics/ML_ACR/AlexData/SNP_Data_Combine/2_MakeMutatedFasta.py \
+-FastaFile /scratch/sb14489/8.ML_ACR/1.MaizeGenotypes_Alex/0.SNPData/"${Fasta[SLURM_ARRAY_TASK_ID]}" \
+-SNPFile /scratch/sb14489/8.ML_ACR/1.MaizeGenotypes_Alex/0.SNPData/"${SNPFiles[SLURM_ARRAY_TASK_ID]}"
