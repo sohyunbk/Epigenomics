b03b221f52b50f34473b68d79b4b2ec5ddb48d44
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-02-19 18:52:13 -0500

    Update MakeGeneBedFilesIamInterestedIn.R

--- scATAC-seq/0_CoreScript/Viualization/MakeGeneBedFilesIamInterestedIn.R
+++ scATAC-seq/0_CoreScript/Viualization/MakeGeneBedFilesIamInterestedIn.R
@@ -2,7 +2,7 @@
 ## The file should be: chr	start	end	geneID	name	type
 GeneBed <- read.table("/scratch/sb14489/0.Reference/Maize_B73/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1_OnlyGene_Chr_AddGeneSymbol.bed",header=FALSE)
 head(GeneBed)
-GeneBed <- GeneBed[grep("^(wox|arftf)", GeneBed$V10, ignore.case = TRUE), ]
+GeneBed <- GeneBed[grep("^(wox|arftf|pin)", GeneBed$V10, ignore.case = TRUE), ]
 
 OutFile <- data.frame(chr =GeneBed$V1,
                       start=GeneBed$V2,
@@ -13,4 +13,4 @@ OutFile <- data.frame(chr =GeneBed$V1,
 OutFile$type <- gsub("\\d", "", OutFile$type)
 write.table(OutFile,
             "/scratch/sb14489/3.scATAC/0.Data/MarkerGene/ARF_WOX.txt",
-            quote=F, row.names=T, col.names=T, sep="\t")
+            quote=F, row.names=F, col.names=T, sep="\t")
