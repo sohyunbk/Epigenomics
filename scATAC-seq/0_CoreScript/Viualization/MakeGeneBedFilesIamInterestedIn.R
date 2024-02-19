9f4a511e76381089ccf0ad5c2b891c1e798d9ff1
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-02-19 12:26:43 -0500

    ss

--- scATAC-seq/0_CoreScript/Viualization/MakeGeneBedFilesIamInterestedIn.R
+++ scATAC-seq/0_CoreScript/Viualization/MakeGeneBedFilesIamInterestedIn.R
@@ -1,1 +1,16 @@
-GeneInfo <- read.table("/scratch/sb14489/0.Reference/Maize_B73/Zm00001eb.1.fulldata.Curated.txt", fill = TRUE,header=TRUE)
+## Load bed File
+## The file should be: chr	start	end	geneID	name	type
+GeneBed <- read.table("/scratch/sb14489/0.Reference/Maize_B73/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1_OnlyGene_Chr_AddGeneSymbol.bed",header=FALSE)
+head(GeneBed)
+GeneBed <- GeneBed[grep("^(wox|arftf)", GeneBed$V10, ignore.case = TRUE), ]
+
+OutFile <- data.frame(chr =GeneBed$V1,
+                      start=GeneBed$V2,
+                      end=GeneBed$V3,
+                      geneID=GeneBed$V4,
+                      name=GeneBed$V10,
+                      type=GeneBed$V10)
+OutFile$type <- gsub("\\d", "", OutFile$type)
+write.table(OutFile,
+            "/scratch/sb14489/3.scATAC/0.Data/MarkerGene/ARF_WOX.txt",
+            quote=F, row.names=T, col.names=T, sep="\t")
--- scATAC-seq/1_OriginalSamples/6-1-2_MarkerGene_tfidf.sh
+++ scATAC-seq/1_OriginalSamples/6-1-2_MarkerGene_tfidf.sh
@@ -9,7 +9,7 @@
 #SBATCH --error=/scratch/sb14489/0.log/6_MarkerGene.%j.err    # Standard error log
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=Sohyun.Bang@uga.edu  # Where to send mail
-#SBATCH --array=0-5
+#SBATCH --array=0-7
 
 source activate r_env
 
@@ -18,38 +18,50 @@ rel2_TopDenovoMarkersA619
 Bif3_IncludingZmCLE7
 Bif3_TopDenovoMarkersA619
 A619_IncludingZmCLE7
-A619_TopDenovoMarkersA619)
+A619_TopDenovoMarkersA619
+A619_WOX_ARF
+Bif3_WOX_ARF)
 FileNames=(rel2_includingZmCLE7
 rel2_TopDenovoMarkersA619
 Bif3_IncludingZmCLE7
 Bif3_TopDenovoMarkersA619
 A619_IncludingZmCLE7
-A619_TopDenovoMarkersA619)
+A619_TopDenovoMarkersA619
+A619_WOX_ARF
+Bif3_WOX_ARF)
 Metas=(6.Annotation/0.AnnotatedMeta/rel2/rel2_AnnV4.txt
 6.Annotation/0.AnnotatedMeta/rel2/rel2_AnnV4.txt
 6.Annotation/0.AnnotatedMeta/Bif3/Bif3_AnnV3_metadata.txt
 6.Annotation/0.AnnotatedMeta/Bif3/Bif3_AnnV3_metadata.txt
 6.Annotation/0.AnnotatedMeta/A619/Ref_AnnV4_metadata.txt
-6.Annotation/0.AnnotatedMeta/A619/Ref_AnnV4_metadata.txt)
+6.Annotation/0.AnnotatedMeta/A619/Ref_AnnV4_metadata.txt
+6.Annotation/0.AnnotatedMeta/A619/Ref_AnnV4_metadata.txt
+6.Annotation/0.AnnotatedMeta/Bif3/Bif3_AnnV3_metadata.txt)
 GAs=(GA_rel2_includingZmCLE7.txt
 GA_rel2_includingZmCLE7.txt
 GA_Bif3_Re.txt
 GA_Bif3_Re.txt
 GA_A619_Re.txt
-GA_A619_Re.txt)
+GA_A619_Re.txt
+GA_A619_Re.txt
+GA_Bif3_Re.txt)
 PCAs=(Organelle5Per_CombineLater/rel2/rel2_Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100_RemoveBLonlyMitoChloroChIP.AfterHarmony.PCA.txt
 Organelle5Per_CombineLater/rel2/rel2_Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100_RemoveBLonlyMitoChloroChIP.AfterHarmony.PCA.txt
 Organelle5Per_CombineLater/bif3/bif3_Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100_RemoveBLonlyMitoChloroChIP.AfterHarmony.PCA.txt
 Organelle5Per_CombineLater/bif3/bif3_Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100_RemoveBLonlyMitoChloroChIP.AfterHarmony.PCA.txt
 Ref_AfterMt0.5Cutoff/Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100/Ref_RemoveBLonlyMitoChloroChIP.REF_CELLs.reduced_dimensions.txt
 Ref_AfterMt0.5Cutoff/Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100/Ref_RemoveBLonlyMitoChloroChIP.REF_CELLs.reduced_dimensions.txt
+Ref_AfterMt0.5Cutoff/Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100/Ref_RemoveBLonlyMitoChloroChIP.REF_CELLs.reduced_dimensions.txt
+Organelle5Per_CombineLater/bif3/bif3_Tn5Cut1000_Binsize500_Mt0.05_MinT0.01_MaxT0.05_PC100_RemoveBLonlyMitoChloroChIP.AfterHarmony.PCA.txt
 )
 Markers=(230426_EarMarker.txt
 231113_Top5DenovoGenesinA619_NoRedundant.txt
 230426_EarMarker.txt
 231113_Top5DenovoGenesinA619_NoRedundant.txt
 230426_EarMarker.txt
-231113_Top5DenovoGenesinA619_NoRedundant.txt)
+231113_Top5DenovoGenesinA619_NoRedundant.txt
+ARF_WOX.txt
+ARF_WOX.txt)
 
 Rscript /home/sb14489/Epigenomics/scATAC-seq/0_CoreScript/Annotation_Cluster/MarkerGenes_UMAPVisual.R \
 --WD /scratch/sb14489/3.scATAC/2.Maize_ear/6.Annotation/1.MarkerGene/"${WDNames[SLURM_ARRAY_TASK_ID]}" \
