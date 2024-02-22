8d504e41f4e8b2a01f7497befde8a4970c980fd8
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-02-22 17:13:09 -0500

    Update CPMNormalization.sh

--- DAP-seq/CPMNormalization.sh
+++ DAP-seq/CPMNormalization.sh
@@ -1,7 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=MetaPlot        # Job name
+#SBATCH --partition=schmitz_hm_p             # Partition (queue) name
+#SBATCH --ntasks=1                    # Run a single task
+#SBATCH --cpus-per-task=20             # Number of CPU cores per task
+#SBATCH --mem=90gb                   # Job memory request
+#SBATCH --time=01:50:00               # Time limit hrs:min:sec
+#SBATCH --output=/scratch/sb14489/0.log/MetaPlot.%j.out   # Standard output log
+#SBATCH --error=/scratch/sb14489/0.log/MetaPlot.%j.err    # Standard error log
+
 ml SAMtools/1.16.1-GCC-11.3.0
-samtools sort -@4 -o HB67_WUS1_B73v5_Q30.sorted.bam HB67_WUS1_B73v5_Q30.bam
+ml BEDTools/2.30.0-GCC-12.2.0
+
+cd /scratch/sb14489/7.DAPorChIP/DAPseq_WUS/WUS1_fastq_Mapped
+
+source  acitivate Jbrowse
+
+samtools sort -@24 -o HB67_WUS1_B73v5_Q30.sorted.bam HB67_WUS1_B73v5_Q30.bam
 samtools index HB67_WUS1_B73v5_Q30.sorted.bam
-genomeCoverageBed -ibam HB67_WUS1_B73v5_Q30.sorted.bam -bg -split > coverage.bedGraph
-SCALE_FACTOR=$(echo "scale=6; 1/(${TOTAL_MAPPED_READS}/1000000)" | bc)
-awk -v scale=${SCALE_FACTOR} 'BEGIN{OFS="\t"} {print $1,$2,$3,$4*scale}' coverage.bedGraph > scaled_coverage.bedGraph
-bedGraphToBigWig scaled_coverage.bedGraph chrom.sizes output.bw
+
+##
+bedtools genomecov -ibam HB67_WUS1_B73v5_Q30.sorted.bam -bg > HB67_WUS1_B73v5_Q30.coverage.bedGraph
+
+
+TOTAL_READS=$(samtools view -F 260 -c HB67_WUS1_B73v5_Q30.sorted.bam)
+echo $TOTAL_READS
+SCALE_FACTOR=$(echo "scale=6; 1/(${TOTAL_READS}/1000000)" | bc)
+awk -v scale=${SCALE_FACTOR} 'BEGIN{OFS="\t"} {print $1,$2,$3,$4*scale}' HB67_WUS1_B73v5_Q30.coverage.bedGraph > HB67_WUS1_B73v5_Q30.CPM.coverage.bedGraph
+bedGraphToBigWig HB67_WUS1_B73v5_Q30.CPM.coverage.bedGraph chrom.sizes HB67_WUS1_B73v5_Q30.CPM.bw
