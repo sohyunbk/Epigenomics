dbea14948d85a37e498185dc16dd16aac528e244
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-03-22 13:22:57 -0400

    Update 3_Bowtie2_PairedEnd.sh

--- Cut_and_Tag/3_Bowtie2_PairedEnd.sh
+++ Cut_and_Tag/3_Bowtie2_PairedEnd.sh
@@ -10,7 +10,7 @@
 #SBATCH --mail-type=BEGIN,END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --array=0-7
 
-module load Bowtie2/2.4.5-GCC-10.2.0
+module load Bowtie2/2.5.2-GCC-11.3.0
 Sample=(SRR21185825 SRR21228818 SRR21228819 SRR21228820 SRR21228821 SRR21228813 SRR21228814 SRR21228816 SRR21228817)
 
 #bowtie2-build --threads 20 /scratch/sb14489/0.Reference/TAIR10/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa /scratch/sb14489/0.Reference/TAIR10/TAIR10
