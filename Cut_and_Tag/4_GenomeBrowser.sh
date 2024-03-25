add7fa34a6ffb9131c458366d1142de2ac057b4e
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-03-25 15:16:31 -0400

    Update 4_GenomeBrowser.sh

--- Cut_and_Tag/4_GenomeBrowser.sh
+++ Cut_and_Tag/4_GenomeBrowser.sh
@@ -19,8 +19,7 @@ module load BEDTools/2.30.0-GCC-12.2.0
 Sample=(SRR21185825 SRR21228818 SRR21228819 SRR21228820 SRR21228821 SRR21228813 SRR21228814 SRR21228816 SRR21228817)
 
 #bowtie2-build --threads 20 /scratch/sb14489/0.Reference/TAIR10/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa /scratch/sb14489/0.Reference/TAIR10/TAIR10
-cd /scratch/sb14489/7.DAPorChIP/CUTandTAG/3.Bowtie2
 
 bash /home/sb14489/Epigenomics/Jbrowse/Make_JBrowseUploadFiles.sh -Step BamTobw  \
  -Fai /scratch/sb14489/0.Reference/Maize_B73/Zm-B73-REFERENCE-NAM-5.0_MtPtAdd_Rsf.fa.fai \
-  -bam "${Sample[SLURM_ARRAY_TASK_ID]}"_bowtie2_algn.bam
+  -bam /scratch/sb14489/7.DAPorChIP/CUTandTAG/3.Bowtie2/"${Sample[SLURM_ARRAY_TASK_ID]}"_bowtie2_algn.bam
