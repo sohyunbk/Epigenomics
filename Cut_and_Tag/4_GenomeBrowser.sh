80e6dbdd021d362c169021d190b5a223306e6124
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-03-25 16:21:29 -0400

    Update 4_GenomeBrowser.sh

--- Cut_and_Tag/4_GenomeBrowser.sh
+++ Cut_and_Tag/4_GenomeBrowser.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=Jbrowse        # Job name
 #SBATCH --partition=highmem_p         # Partition (queue) name
 #SBATCH --ntasks=1                    # Run a single task
-#SBATCH --cpus-per-task=2            # Number of CPU cores per task
+#SBATCH --cpus-per-task=20            # Number of CPU cores per task
 #SBATCH --mem=100gb                   # Job memory request #For normal fastq : 600gb
 #SBATCH --time=3:00:00               # Time limit hrs:min:sec #For normal fastq : more than at least five days with 14 cpu 80 hours
 #SBATCH --output=/scratch/sb14489/0.log/Jbrowse.%j.out   # Standard output log
