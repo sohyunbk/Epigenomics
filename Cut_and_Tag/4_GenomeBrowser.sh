d259338bbdc0553d4a978c2091fbd80df0c45ce2
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-03-25 14:26:09 -0400

    Update 4_GenomeBrowser.sh

--- Cut_and_Tag/4_GenomeBrowser.sh
+++ Cut_and_Tag/4_GenomeBrowser.sh
@@ -10,7 +10,12 @@
 #SBATCH --mail-type=BEGIN,END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --array=0-7
 
-module load Bowtie2/2.5.2-GCC-11.3.0
+ml Anaconda3/2020.02
+source activate /home/sb14489/.conda/envs/Jbrowse
+
+module load SAMtools/1.16.1-GCC-11.3.0
+module load BEDTools/2.30.0-GCC-12.2.0
+
 Sample=(SRR21185825 SRR21228818 SRR21228819 SRR21228820 SRR21228821 SRR21228813 SRR21228814 SRR21228816 SRR21228817)
 
 #bowtie2-build --threads 20 /scratch/sb14489/0.Reference/TAIR10/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa /scratch/sb14489/0.Reference/TAIR10/TAIR10
