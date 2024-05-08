820c004aa70de1734754c67a1896bc82cb8c4b6c
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-05-08 16:00:30 -0400

    ss

--- scATAC-seq/0_CoreScript/Alignment/2-1_Alignment_CellRangerv2.sh
+++ scATAC-seq/0_CoreScript/Alignment/2-1_Alignment_CellRangerv2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=Alignment        # Job name
-#SBATCH --partition=highmem_p         # Partition (queue) name
+#SBATCH --partition=schmitz_hm_p         # Partition (queue) name
 #SBATCH --ntasks=1                    # Run a single task
-#SBATCH --cpus-per-task=14             # Number of CPU cores per task
-#SBATCH --mem=600gb                   # Job memory request #For normal fastq : 600gb
+#SBATCH --cpus-per-task=32             # Number of CPU cores per task
+#SBATCH --mem=500gb                   # Job memory request #For normal fastq : 600gb
 #SBATCH --time=30:00:04               # Time limit hrs:min:sec #For normal fastq : 80 hours
 #SBATCH --output=/scratch/sb14489/0.log/2_Mapping.%j.out   # Standard output log
 #SBATCH --error=/scratch/sb14489/0.log/2_Mapping.%j.err    # Standard error log
