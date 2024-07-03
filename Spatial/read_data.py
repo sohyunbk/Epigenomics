12546093ea5d6a7c7b9ba20144c273a1c177f6c8
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-07-03 11:59:16 -0400

    Update read_data.py

--- Spatial/read_data.py
+++ Spatial/read_data.py
@@ -21,4 +21,4 @@ adata.var['Mt'] = adata.var_names.str.startswith('chrMt')
 adata.var['PT'] = adata.var_names.str.startswith('chrPt')
 sc.pp.calculate_qc_metrics(adata, qc_vars=['Mt', 'PT'], inplace=True)
 
-adata.write(f"adata.h5ad")
+adata.write("adata.h5ad")
