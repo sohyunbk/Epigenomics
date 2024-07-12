7a393254da0a439a6bda478c0c8386ee7e38637f
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-07-12 12:11:27 -0400

    ss

--- scATAC-seq/1_OriginalSamples/7-1_CombineBedFiles_andSorts.sh
+++ scATAC-seq/1_OriginalSamples/7-1_CombineBedFiles_andSorts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=CombineBedfiles        # Job name
-#SBATCH --partition=highmem_p         # Partition (queue) name
+#SBATCH --partition=schmitz_hm_p         # Partition (queue) name
 #SBATCH --ntasks=1                    # Run a single task
 #SBATCH --cpus-per-task=1             # Number of CPU cores per task
 #SBATCH --mem=500gb                   # Job memory request
