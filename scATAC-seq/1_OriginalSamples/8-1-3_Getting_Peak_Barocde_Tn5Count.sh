7c2bedb58f97752b4fd42e170694f68e017c5ac2
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-07-15 16:11:07 -0400

    Update 8-1-3_Getting_Peak_Barocde_Tn5Count.sh

--- scATAC-seq/1_OriginalSamples/8-1-3_Getting_Peak_Barocde_Tn5Count.sh
+++ scATAC-seq/1_OriginalSamples/8-1-3_Getting_Peak_Barocde_Tn5Count.sh
@@ -7,25 +7,29 @@
 #SBATCH --time=20:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/sb14489/0.log/7_Peak_Barcode_Tn5.%j.out   # Standard output log
 #SBATCH --error=/scratch/sb14489/0.log/7_Peak_Barcode_Tn5.%j.err    # Standard error log
-#SBATCH --array=0-1
+#SBATCH --array=0-10
 
 ## If the file is small it takes 2 hour
 ## If it's the big file... it takes 6-7 hours
-Tn5BedNames=(1_A619 3_bif3 3_bif3 1_A619 2_rel2 1_A619)
+Tn5BedNames=(1_A619 3_bif3 3_bif3 1_A619 2_rel2 1_A619 4_relk1 1_A619)
 PeakFiles=(
 A619/A619.500bp_peaks_Intergenic.bed
 Bif3/Bif3.500bp_peaks_Intergenic.bed
 A619_Bif3_500bpCommonPeak/ComA619Bif3.unique500bpPeaks.bed
 A619_Bif3_500bpCommonPeak/ComA619Bif3.unique500bpPeaks.bed
 A619_rel2_500bpCommonPeak/ComA619rel2.unique500bpPeaks.bed
-A619_rel2_500bpCommonPeak/ComA619rel2.unique500bpPeaks.bed)
+A619_rel2_500bpCommonPeak/ComA619rel2.unique500bpPeaks.bed
+A619_relk1_500bpCommonPeak/ComA619relk1.unique500bpPeaks.bed
+A619_relk1_500bpCommonPeak/ComA619relk1.unique500bpPeaks.bed)
 OutFileNames=(
 A619/A619Peak500bp_A619Barcode_Tn5Count
 Bif3/Bif3Peak500bp_Bif3Barcode_Tn5Count
 A619_Bif3_500bpCommonPeak/ComA619Bif3_Bif3Barcode_Tn5Count
 A619_Bif3_500bpCommonPeak/ComA619Bif3_A619Barcode_Tn5Count
 /A619_rel2_500bpCommonPeak/ComPeakA619rel2_Rel2Barcode_Tn5Count
-/A619_rel2_500bpCommonPeak/ComPeakA619rel2_A619Barcode_Tn5Count)
+/A619_rel2_500bpCommonPeak/ComPeakA619rel2_A619Barcode_Tn5Count
+/A619_relk1_500bpCommonPeak/ComPeakA619relk1_Relk1Barcode_Tn5Count
+/A619_relk1_500bpCommonPeak/ComPeakA619relk1_Relk1Barcode_Tn5Count)
 
 ml Anaconda3/2022.10
 source activate r_env
