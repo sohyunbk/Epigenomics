624474af5f750b60e209c441f87d68358b3999a2
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-07-15 16:01:54 -0400

    Update 8-1-2_ClassifyPeaks.sh

--- scATAC-seq/1_OriginalSamples/8-1-2_ClassifyPeaks.sh
+++ scATAC-seq/1_OriginalSamples/8-1-2_ClassifyPeaks.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=1:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/sb14489/0.log/ClassifyingPeaks.%j.out   # Standard output log
 #SBATCH --error=/scratch/sb14489/0.log/ClassifyingPeaks.%j.err    # Standard error log
-#SBATCH --array=0-1
+#SBATCH --array=0-2
 
 source activate r_env
 
