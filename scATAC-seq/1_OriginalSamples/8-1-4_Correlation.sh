acbff456fa50fd0141599039650ac0b996f83ff9
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-07-16 14:39:07 -0400

    Update 8-1-4_Correlation.sh

--- scATAC-seq/1_OriginalSamples/8-1-4_Correlation.sh
+++ scATAC-seq/1_OriginalSamples/8-1-4_Correlation.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=10:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/sb14489/0.log/Correlation.%j.out   # Standard output log
 #SBATCH --error=/scratch/sb14489/0.log/Correlation.%j.err    # Standard error log
-#SBATCH --array=0-1
+#SBATCH --array=0-2
 
 
 source activate r_env
