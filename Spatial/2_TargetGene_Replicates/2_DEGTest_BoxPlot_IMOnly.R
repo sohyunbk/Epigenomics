29dc92adff526cc656914b04c683e14428a0c11c
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-08-21 19:47:03 -0400

    change

--- Spatial/2_TargetGene_Replicates/2_DEGTest_BoxPlot_IMOnly.R
+++ Spatial/2_TargetGene_Replicates/2_DEGTest_BoxPlot_IMOnly.R
@@ -3,55 +3,44 @@ library(ggplot2)
 
 
 ##### 1) load Data and write the Replicate info!
-file_path <- "/scratch/sb14489/9.spatialRNAseq/3.TargetGene_IMAM/"
+file_path <- "/scratch/sb14489/9.spatialRNAseq/3.TargetGene_IMOnly/"
 
 # WT Re1
 data <- read.csv(paste0(file_path,"A619_B_Cluser5.csv"), row.names = 1, header = TRUE)
 #data <- as.data.table(data)
 WT_Re1 <- colSums(data)
-
 #WT Re2
-data <- read.csv(paste0(file_path,"A619_C_Cluser1.csv"), row.names = 1, header = TRUE)
-WT_Re2_1 <- colSums(data)
 data <- read.csv(paste0(file_path,"A619_C_Cluser7.csv"), row.names = 1, header = TRUE)
+WT_Re2_1 <- colSums(data)
+data <- read.csv(paste0(file_path,"A619_D_WT_Cluser15.csv"), row.names = 1, header = TRUE)
 WT_Re2_2 <- colSums(data)
-data <- read.csv(paste0(file_path,"A619_D_WTCZOnly_new.csv"), row.names = 1, header = TRUE)
-WT_Re2_3 <- colSums(data)
-WT_Re2 <- WT_Re2_1+WT_Re2_2+WT_Re2_3
+WT_Re2 <- WT_Re2_1+WT_Re2_2
 
 # Bif3 Re1
-data <- read.csv(paste0(file_path,"A619_D_Bif3CZOnly.csv"), row.names = 1, header = TRUE)
+data <- read.csv(paste0(file_path,"A619_D_Bif3_Cluser6.csv"), row.names = 1, header = TRUE)
 Bif3_Re1 <- colSums(data)
 
 # Bif3 Re2
 data <- read.csv(paste0(file_path,"Bif3_A_Cluser1.csv"), row.names = 1, header = TRUE)
-Bif3_Re2_1 <- colSums(data)
-data <- read.csv(paste0(file_path,"Bif3_A_Cluser10.csv"), row.names = 1, header = TRUE)
-Bif3_Re2_2 <- colSums(data)
-Bif3_Re2 <- Bif3_Re2_1+Bif3_Re2_2
+Bif3_Re2 <- colSums(data)
 
 # Bif3 Re3
-data <- read.csv(paste0(file_path,"Bif3_B_Cluser5.csv"), row.names = 1, header = TRUE)
-Bif3_Re3_1 <- colSums(data)
+
 data <- read.csv(paste0(file_path,"Bif3_B_Cluser6.csv"), row.names = 1, header = TRUE)
-Bif3_Re3_2 <- colSums(data)
-Bif3_Re3 <- Bif3_Re3_1+Bif3_Re3_2
+Bif3_Re3 <- colSums(data)
 
 # Bif3 Re4
 data <- read.csv(paste0(file_path,"Bif3_C_Cluser11.csv"), row.names = 1, header = TRUE)
 Bif3_Re4_1 <- colSums(data)
-data <- read.csv(paste0(file_path,"Bif3_C_Cluser5.csv"), row.names = 1, header = TRUE)
-Bif3_Re4_2 <- colSums(data)
-data <- read.csv(paste0(file_path,"Bif3_D_Cluser10.csv"), row.names = 1, header = TRUE)
-Bif3_Re4_3 <- colSums(data)
 data <- read.csv(paste0(file_path,"Bif3_D_Cluser7.csv"), row.names = 1, header = TRUE)
 Bif3_Re4_4 <- colSums(data)
-Bif3_Re4 <- Bif3_Re4_1+Bif3_Re4_2+Bif3_Re4_3+Bif3_Re4_4
+Bif3_Re4 <- Bif3_Re4_1+Bif3_Re4_4
 
 CountTable <- data.frame(WT_Re1,WT_Re2,Bif3_Re1,Bif3_Re2,Bif3_Re3,Bif3_Re4)
 CountTable <- CountTable[grep("^Zm00", rownames(CountTable)), ]
 head(CountTable)
 tail(CountTable)
+
 ### EdgeR count info: row - gene column Sample
 group <- factor(c(rep("WT",2), rep("Bif3",4)))
 SampleID <- colnames(CountTable)
@@ -104,10 +93,11 @@ for (i in c(1:nrow(tmm_normalized_selected))) {
 library(cowplot)
 
 final_plot <- plot_grid(plotlist = Plotlist, ncol = 6)
-output_name <- "/scratch/sb14489/9.spatialRNAseq/3.TargetGene_IMAM/Spatial_BoxPlot_11Genes_IMAM.pdf"
+output_name <- "/scratch/sb14489/9.spatialRNAseq/3.TargetGene_IMOnly/Spatial_BoxPlot_11Genes.pdf"
 ggsave(output_name, plot = final_plot,
        width = 14, height = 4,
        units = c('in'), limitsize = FALSE,
        dpi = 300)
+
 
 
