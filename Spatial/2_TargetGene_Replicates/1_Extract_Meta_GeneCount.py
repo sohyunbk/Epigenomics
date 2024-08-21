da583187adc94b4780288f52907e303f1e0cb2e1
Sohyun Bang <56011398+sohyunbk@users.noreply.github.com>
2024-08-21 11:21:31 -0400

    Change

--- Spatial/2_TargetGene_Replicates/1_Extract_Meta_GeneCount.py
+++ Spatial/2_TargetGene_Replicates/1_Extract_Meta_GeneCount.py
@@ -37,3 +37,59 @@ def WriteTheCountData(InputDirName,ClusterName,OutputName):
 ### WT - B : Cluster 5
 WriteTheCountData("A619_B","5","A619_B_Cluser5")
 ## WT-C :Cluster 1 + 7
+WriteTheCountData("A619_C","1","A619_C_Cluser1")
+WriteTheCountData("A619_C","7","A619_C_Cluser7")
+
+
+############################################
+## WT-D : WT : special! 
+InputDirName = "A619_D_Res2"
+Adata_raw = sc.read("/scratch/sb14489/9.spatialRNAseq/2.QC_MarkerGene/"+InputDirName+"/adata_raw.h5ad")
+Adata = sc.read("/scratch/sb14489/9.spatialRNAseq/2.QC_MarkerGene/"+InputDirName+"/adata_processed.h5ad")
+#Adata
+Meta = Adata.obs
+Meta
+array_row_list = Meta[Meta['clusters'] == "15"]['array_row'].astype(int).tolist()
+## The max : 38 is upto the WT
+Meta['array_row'] = Meta['array_row'].astype(int)
+Meta['Genotype'] = Meta['array_row'].apply(lambda x: 'WT' if x <= 39 else 'Bif3')
+
+## WT CentralZone
+CZ1 = Meta[(Meta['clusters'] == "15") & (Meta['Genotype'] == "WT")]
+CZ2 = Meta[(Meta['clusters'] == "1") & (Meta['Genotype'] == "WT")]
+CZ3 = Meta[(Meta['clusters'] == "7") & (Meta['Genotype'] == "WT")]
+CZ = pd.concat([CZ1,CZ2,CZ3], axis=0)
+##################################
+cell_gene_count_matrix = Adata_raw.X
+cell_gene_count_matrix_dense = cell_gene_count_matrix.toarray() if isinstance(cell_gene_count_matrix, scipy.sparse.spmatrix) else cell_gene_count_matrix
+cell_gene_count_raw = pd.DataFrame(cell_gene_count_matrix_dense, index=Adata_raw.obs.index, columns=Adata_raw.var.index)
+#print(cell_gene_count_raw)
+#### Get the CZ cells from the table ###
+barcode_set = CZ.index.tolist()
+CZ_Count = cell_gene_count_raw.loc[cell_gene_count_raw.index.isin(barcode_set)]
+CZ_Count.to_csv('/scratch/sb14489/9.spatialRNAseq/3.TargetGene/A619_D_WTCZOnly.csv', index=True, header=True)
+
+##### Bif3 Special
+#Adata
+Meta = Adata.obs
+Meta
+array_row_list = Meta[Meta['clusters'] == "15"]['array_row'].astype(int).tolist()
+## The max : 38 is upto the WT
+Meta['array_row'] = Meta['array_row'].astype(int)
+Meta['Genotype'] = Meta['array_row'].apply(lambda x: 'WT' if x <= 39 else 'Bif3')
+
+## WT CentralZone
+CZ1 = Meta[(Meta['clusters'] == "6") & (Meta['Genotype'] == "Bif3")]
+CZ2 = Meta[(Meta['clusters'] == "7") & (Meta['Genotype'] == "Bif3")]
+CZ = pd.concat([CZ1,CZ2], axis=0)
+##################################
+cell_gene_count_matrix = Adata_raw.X
+cell_gene_count_matrix_dense = cell_gene_count_matrix.toarray() if isinstance(cell_gene_count_matrix, scipy.sparse.spmatrix) else cell_gene_count_matrix
+cell_gene_count_raw = pd.DataFrame(cell_gene_count_matrix_dense, index=Adata_raw.obs.index, columns=Adata_raw.var.index)
+#print(cell_gene_count_raw)
+#### Get the CZ cells from the table ###
+barcode_set = CZ.index.tolist()
+CZ_Count = cell_gene_count_raw.loc[cell_gene_count_raw.index.isin(barcode_set)]
+CZ_Count.to_csv('/scratch/sb14489/9.spatialRNAseq/3.TargetGene/A619_D_Bif3CZOnly.csv', index=True, header=True)
+
+
